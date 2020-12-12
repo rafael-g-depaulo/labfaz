72ac7989b370308c090e4ab6af53e7f400bd939e
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2020-12-12 12:19:14 -0300

    feat: Create User and Sessions routes paths

--- apps/api/src/Routes/index.ts
+++ apps/api/src/Routes/index.ts
@@ -3,6 +3,8 @@ import { Connection } from "typeorm"
 
 // sub routers
 import AnimalExampleRouter from "./AnimalExample"
+import UserRouter from "./User"
+import SessionRouter from './Session'
 
 export interface RequestHandler<ReqBody = {}, Params = {}> {
   (req: Request<Params, {}, ReqBody>, res: Response, next?: NextFunction): any,
@@ -27,5 +29,7 @@ type BaseRouterDeps = {
 const Routes: Router<BaseRouterDeps> = ({ conn }, options = defaultOptions) => express.Router(options)
   .get("/ping", (_, res) => res.json("pong"))
   .use("/animal-example", AnimalExampleRouter({ conn }, options))
+  .use("/user", UserRouter({ conn }, options))
+  .use("/sessions", SessionRouter({ conn }, options))
 
 export default Routes
