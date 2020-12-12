4800f023bf2ac4f53d9d3730748f24a5c1cc57e3
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2020-12-12 12:17:17 -0300

    feat: Create Session Route
    If this commit is applied it makes apply
    user login in the application generating
    jwt token
    
    https://github.com/rafael-g-depaulo/labfaz/issues/4

---
+++ apps/api/src/Routes/Session/CreateSession.ts
@@ -0,0 +1,49 @@
+import { DeepPartial } from "typeorm"
+import { sign } from "jsonwebtoken"
+import { RequestHandler } from "Routes"
+
+import User from "Entities/User"
+import UserRepository from "Repository/UserRepository"
+import authConfig from "Config/auth"
+
+interface CreateSessionInterface {
+  UserRepo: UserRepository,
+}
+
+interface IUser {
+  email: string
+  password: string
+}
+
+export const CreateSession: (deps: CreateSessionInterface) => RequestHandler<DeepPartial<User>> = ({
+  UserRepo,
+}: CreateSessionInterface) => async (req, res) => {
+
+  const {
+    email,
+    password
+  } = req.body as IUser
+
+  const user = await UserRepo.findByEmail(email)
+
+  if (!user) {
+    return res.status(401).json({ error: "Incorrect email/password combination." })
+  }
+
+  const passwordMatched = await UserRepo.compareHash(password, user.password)
+
+  if (!passwordMatched) {
+    return res.status(401).json({ error: "Incorrect email/password combination." })
+  }
+
+  const { secret, expiresIn } = authConfig.jwt
+
+  const token = sign({}, secret, {
+    subject: user.id,
+    expiresIn,
+  })
+  
+  return res.status(200).json({ token, user })
+}
+
+export default CreateSession
