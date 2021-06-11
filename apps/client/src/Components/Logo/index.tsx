d1176677c43e6474bc8c4b5e6eea4d86237cb174
priscila-eng <priscilaangell123@gmail.com>
2021-06-11 15:51:42 -0300

    ✨ Implements Logo Component

---
+++ apps/client/src/Components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import LogoImg from './Logo_Sample.png'
+import { Image } from './style'
+
+const Logo = () => {
+  return <Image src={LogoImg} alt='logo' />
+}
+
+export default Logo
