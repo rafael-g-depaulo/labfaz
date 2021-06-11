f24a9bf49033dc789a2fe829b9126dc24a5fc273
priscila-eng <priscilaangell123@gmail.com>
2021-06-11 17:08:58 -0300

    ✅ Refactor test and add test of logo link

--- apps/client/src/Components/Header/index.test.tsx
+++ apps/client/src/Components/Header/index.test.tsx
@@ -1,55 +1,48 @@
 import React from 'react'
-import Render from 'Utils/render'
-import { fireEvent } from '@testing-library/react'
+import render from 'Utils/render'
 
 import Header from './index'
 
 it('renders header component', () => {
-  expect(() => Render(<Header />)).not.toThrow()
+  expect(() => render(<Header />)).not.toThrow()
 })
 
 describe('Check links succesfully redirects to another page', () => {
-  const { getByText } = Render(<Header />);
-  it('check link of about us', () => {
-    const link = getByText('Quem Somos');
-    fireEvent.click(link);
+  const { getByText, getByTestId, getByRole } = render(<Header />);
+  it('check link of home page and image logo', () => {
+    expect(getByTestId('home').closest('a')).toHaveAttribute('href', '/home')
+    expect(getByRole('img')).toHaveAttribute('alt', 'logo')
+  })
+
+  it('check link of about us page', () => {
     expect(getByText('Quem Somos').closest('a')).toHaveAttribute('href', '/about')
   })
-  it('check link of list courses', () => {
-    const link = getByText('Cursos');
-    fireEvent.click(link);
+
+  it('check link of list courses page', () => {
     expect(getByText('Cursos').closest('a')).toHaveAttribute('href', '/classes')
   })
-  it('check link of calendar', () => {
-    const link = getByText('Agenda');
-    fireEvent.click(link);
+
+  it('check link of calendar page', () => {
     expect(getByText('Agenda').closest('a')).toHaveAttribute('href', '/calendar')
   })
-  it('check link of blog', () => {
-    const link = getByText('Blog');
-    fireEvent.click(link);
+
+  it('check link of blog page', () => {
     expect(getByText('Blog').closest('a')).toHaveAttribute('href', '/blog')
   })
-  it('check link of professionals', () => {
-    const link = getByText('Banco de Profissionais');
-    fireEvent.click(link);
-    expect(getByText('Banco de Profissionais').closest('a')).toHaveAttribute('href', '/professionals')
+
+  it('check link of professionals page', () => {
+    expect(getByText('Banco de Profissionais').closest('a'))
+    .toHaveAttribute('href', '/professionals')
   })
 })
 
-describe('Check user session buttons', () => {
-  
+describe('Check user session links', () => {
+  const { getByText } = render(<Header />);
   it('check signin button', () => {
-    const { getByText } = Render(<Header />);
-    const link = getByText('Entrar');
-    fireEvent.click(link);
     expect(getByText('Entrar').closest('a')).toHaveAttribute('href', '/login')
   })
 
   it('check sign up button', () => {
-    const { getByText } = Render(<Header />);
-    const link = getByText('Cadastre-se');
-    fireEvent.click(link);
     expect(getByText('Cadastre-se').closest('a')).toHaveAttribute('href', '/register')
   })
 })
