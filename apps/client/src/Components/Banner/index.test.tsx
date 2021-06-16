cca1e09420a4cb29a895b18fccc3dcdc834d5bc4
priscila-eng <priscilaangell123@gmail.com>
2021-06-16 10:46:48 -0300

    🐛 Add BrowserRouter because Link tag

--- apps/client/src/Components/Banner/index.test.tsx
+++ apps/client/src/Components/Banner/index.test.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+
 import Render from 'Utils/render'
-import { fireEvent } from '@testing-library/react'
 import Banner from './'
 
 it('renders header component', () => {
-  expect(() => Render(<Banner />)).not.toThrow()
+  expect(() => Render(<BrowserRouter><Banner /></BrowserRouter>)).not.toThrow()
 })
 
 describe('Check content of Banner component', () => {
-  const { getByText } = Render(<Banner />);
+  const { getByText } = Render(<BrowserRouter><Banner /></BrowserRouter>);
   it('check render of title', () => {
     expect(getByText('LABFAZ')).toHaveTextContent('LABFAZ')
   })
@@ -17,8 +18,6 @@ describe('Check content of Banner component', () => {
     expect(subtitle).toHaveTextContent('Lorem')
   })
   it('check link of know more', () => {
-    const link = getByText('SAIBA MAIS');
-    fireEvent.click(link);
-    expect(link.closest('a')).toHaveAttribute('href', '/about')
+    expect(getByText('SAIBA MAIS').closest('a')).toHaveAttribute('href', '/about')
   })
 })
