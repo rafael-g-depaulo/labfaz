82bc2077cf58d7690b5499d3747bb1012f85e67e
priscila-eng <priscilaangell123@gmail.com>
2021-06-16 10:45:11 -0300

    ⚡️ Add BrowserRouter because Link tag

--- apps/client/src/Components/Banner/Banner.stories.tsx
+++ apps/client/src/Components/Banner/Banner.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom';
 
 import Banner from './'
 
 storiesOf("Components/Banner", module)
   .addParameters({ component: Banner })
-  .add("Banner", () => <Banner />)
+  .add("banner", () => <BrowserRouter><Banner /></BrowserRouter>)
