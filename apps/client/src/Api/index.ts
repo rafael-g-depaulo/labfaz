17e9c7914281f707f59f4245a6dc54a37308205e
priscila-eng <priscilaangell123@gmail.com>
2021-06-16 10:44:29 -0300

    🐛 Fix comments file

--- apps/client/src/Api/index.ts
+++ apps/client/src/Api/index.ts
@@ -1,13 +1,13 @@
-// import axios from "axios"
+import axios from "axios"
 
-// import { getApiUrl, getStrapiUrl } from "@labfaz/server-conn-info"
+import { getApiUrl, getStrapiUrl } from "@labfaz/server-conn-info"
 
-// // create axios instance to connect to main server
-// export const api = axios.create({
-//   baseURL: getApiUrl(),
-// })
+// create axios instance to connect to main server
+export const api = axios.create({
+  baseURL: getApiUrl(),
+})
 
-// // create axios instance to connect to strapi server
-// export const strapi = axios.create({
-//   baseURL: getStrapiUrl(),
-// })
+// create axios instance to connect to strapi server
+export const strapi = axios.create({
+  baseURL: getStrapiUrl(),
+})
