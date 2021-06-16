ca24b807650cad7878b3330d557891b368781458
priscila-eng <priscilaangell123@gmail.com>
2021-06-16 10:46:48 -0300

    🐛 Add BrowserRouter because Link tag

--- apps/client/src/Components/Header/index.test.tsx
+++ apps/client/src/Components/Header/index.test.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import render from 'Utils/render'
+import { BrowserRouter } from 'react-router-dom';
 
+import render from 'Utils/render'
 import Header from './index'
 
 it('renders header component', () => {
-  expect(() => render(<Header />)).not.toThrow()
+  expect(() => render(<BrowserRouter><Header /></BrowserRouter>)).not.toThrow()
 })
 
 describe('Check links succesfully redirects to another page', () => {
-  const { getByText, getByTestId, getByRole } = render(<Header />);
+  const { 
+    getByText,
+    getByTestId,
+    getByRole
+  } = render(<BrowserRouter><Header /></BrowserRouter>);
   it('check link of home page and image logo', () => {
-    expect(getByTestId('home').closest('a')).toHaveAttribute('href', '/home')
+    expect(getByTestId('home').closest('a')).toHaveAttribute('href', '/')
     expect(getByRole('img')).toHaveAttribute('alt', 'logo')
   })
 
@@ -37,7 +42,7 @@ describe('Check links succesfully redirects to another page', () => {
 })
 
 describe('Check user session links', () => {
-  const { getByText } = render(<Header />);
+  const { getByText } = render(<BrowserRouter><Header /></BrowserRouter>);
   it('check signin button', () => {
     expect(getByText('Entrar').closest('a')).toHaveAttribute('href', '/login')
   })
