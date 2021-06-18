98912f30defddfa69b844ceca548746bef369f83
priscila-eng <priscilaangell123@gmail.com>
2021-06-18 15:39:01 -0300

    ⚡️ Add YoutubePlayerResponsivo Component

--- apps/client/src/Components/Presentation/index.tsx
+++ apps/client/src/Components/Presentation/index.tsx
@@ -1,43 +1,29 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Container, TextBlock } from './style'
 
-import { YoutubePlayer } from 'Components/YoutubePlayer'
+import { YoutubePlayerResponsive } from 'Components/YoutubePlayerResponsive'
+import Loading from "Components/Loading"
+import { useHomePresentationInfo } from 'Api/Presentation';
 import useWidth from 'Hooks/useWidth';
 
 
-const Presentation = () => {
-  let largura = useWidth();
+export const Presentation: FC = () => {
+  const { data, error, isLoading } = useHomePresentationInfo()
+  const { Title, SubTitle, Video} = data!
+
+  let width = useWidth();
+
+  if (isLoading) return <Loading />
+
+  if (error) return <div>error!</div>
+  console.log(data)
   return (
     <Container>
       <div className="ContainerContent">
-        { largura >= 768 && largura <= 844 ? 
-          (
-            <YoutubePlayer
-              href="https://www.youtube.com/watch?v=gvQSng1aR9s"
-              width="400"
-              height="200"
-            />
-          ) : (
-            largura > 844 && largura <= 1080 ? 
-            <YoutubePlayer
-              href="https://www.youtube.com/watch?v=gvQSng1aR9s"
-              width="500"
-              height="300"
-            /> :
-            <YoutubePlayer
-              width="500"
-              href="https://www.youtube.com/watch?v=gvQSng1aR9s"
-            />
-          )
-        }
+        <YoutubePlayerResponsive href={Video} width={width}/>
         <TextBlock>
-          <h1> Lorem ipsum </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula 
-            eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient 
-            montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
-            pretium quis, sem. Nulla consequat massa quis enim. Donec.
-          </p>
+          <h1> {Title} </h1>
+          <p> {SubTitle} </p>
         </TextBlock>
       </div>
     </Container>
