faf60ce722ce009c4f6296e8ff28f047244bee18
priscila-eng <priscilaangell123@gmail.com>
2021-06-18 15:37:50 -0300

    ⚡️ Change YoutubePlayer Component to typescript

--- apps/client/src/Components/YoutubePlayer/index.tsx
+++ apps/client/src/Components/YoutubePlayer/index.tsx
@@ -1,54 +1,39 @@
-import React from 'react'
-import YouTube from 'react-youtube';
+import React, { FC } from 'react'
+import YouTube from 'react-youtube'
 
-export const YoutubePlayer = ({...props}) => {
+export interface YoutubePlayerProps {
+  height?: number,
+  width?: number,
+  href: string,
+}
 
+export const YoutubePlayer: FC<YoutubePlayerProps> = ({
+  height = 400,
+  width = 1080,
+  href,
+}) => {
   let position, id;
-  const optsWidthHeight = {
-    width: props.width,
-    height: props.height,
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-    },
-  }
-  const optsWidth = {
-    width: props.width,
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-    },
+  const options = {
+    width: `${width}`,
+    height: `${height}`,
+    playerVars: {},
   }
 
-  const optsHeight = {
-    height: props.height,
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-    },
-  }
-
-  const optsDefault = {
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-    },
-  }
-
-  if (props.href.substring(7,15) === "youtu.be") {
-    position = props.href.lastIndexOf('be/')
-    id = props.href.substr(position+3)
-  } else if (props.href.substring(16,21) === "embed") {
-    position = props.href.lastIndexOf('embed/')
-    id = props.href.substr(position+6)
+  if (href.substring(7,15) === "youtu.be") {
+    position = href.lastIndexOf('be/')
+    id = href.substr(position+3)
+  } else if (href.substring(16,21) === "embed") {
+    position = href.lastIndexOf('embed/')
+    id = href.substr(position+6)
   } else {
-    position = props.href.lastIndexOf('watch?v=')  
-    id = props.href.substr(position+8)
+    position = href.lastIndexOf('watch?v=')  
+    id = href.substr(position+8)
   }
+
   return (
     <YouTube
       videoId={id}
-      opts={
-        props.width ? 
-          (props.height ? optsWidthHeight : optsWidth) : 
-          (props.height ? optsHeight : optsDefault)
-      } 
+      opts={options}
     />
   )
 }
