6a649f994ef652a5da2317451a9b4a0341f2d8a9
priscila-eng <priscilaangell123@gmail.com>
2021-06-19 14:41:38 -0300

    💄 Change web style

---
+++ apps/client/src/Components/Header/Web/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+
+import { Container, Navbar, UserSession, Button, NavLink } from './style'
+import Logo from 'Components/Logo'
+
+export const Header: FC = () => {
+  return (
+    <Container>
+      <Link className="home" to="/" data-testid="home">
+        <Logo />
+      </Link>
+      <Navbar>
+        <NavLink to="/about"> Quem Somos </NavLink>
+        <NavLink to="/classes"> Cursos </NavLink>
+        <NavLink to="/calendar"> Agenda </NavLink>
+        <NavLink to="/blog"> Blog </NavLink>
+        <NavLink to="/professionals"> Banco de Profissionais </NavLink>
+      </Navbar>
+      <div className="line" />
+      <UserSession>
+        <Link to='/login'> Entrar </Link>
+          <Button to='/register'> Cadastre-se </Button>
+      </UserSession>
+    </Container>
+  )
+}
+
+export default Header
