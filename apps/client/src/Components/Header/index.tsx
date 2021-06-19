a833e573d737fc29cea6a211863d579d8db63821
priscila-eng <priscilaangell123@gmail.com>
2021-06-19 14:39:50 -0300

    Change header for mobile and web

--- apps/client/src/Components/Header/index.tsx
+++ apps/client/src/Components/Header/index.tsx
@@ -1,29 +1,15 @@
 import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
 
-import { Container, Navbar, UserSession, Button } from './style'
-import Logo from 'Components/Logo'
+import useWidth from 'Hooks/useWidth'
+import Mobile from 'Components/Header/Mobile'
+import Web from 'Components/Header/Web'
 
 export const Header: FC = () => {
-  return (
-    <Container>
-      <Link className="home" to="/" data-testid="home">
-        <Logo />
-      </Link>
-      <Navbar> 
-        <Link to="/about"> Quem Somos </Link>
-        <Link to="/classes"> Cursos </Link>
-        <Link to="/calendar"> Agenda </Link>
-        <Link to="/blog"> Blog </Link>
-        <Link to="/professionals"> Banco de Profissionais </Link>
-      </Navbar>
-      <div className="line" />
-      <UserSession>
-        <Link to='/login'> Entrar </Link>
-        <Button to='/register'> Cadastre-se </Button>
-      </UserSession>
-    </Container>
-  )
+  const width = useWidth()
+
+  if (width <= 660) return <Mobile />
+  
+  else return <Web/>
 }
 
 export default Header
