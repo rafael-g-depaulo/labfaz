2e1ff562a366ad8ac382ed874eff5df0da117abb
Icaro Rezende <icaronr@gmail.com>
2021-06-20 09:14:16 -0300

    💄 updates the GlobalStyles' font to prototype's

--- apps/client/src/GlobalStyles.tsx
+++ apps/client/src/GlobalStyles.tsx
@@ -1,16 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-
+  /* import Cairo Font */
+  @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap');
+  /* @import Inter Font */
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap');
   /* variables */
   html {
     --color-text: black;
     --font-size: 18px;
+    --font-family: 'Inter', 'Cairo', sans-serif;
   }
 
   * {
     box-sizing: border-box;
-    font-family: "Town Text";
+    font-family: var(--font-family);
     font-size: var(--font-size);
     color: var(--colot-text);
   }
@@ -19,7 +23,6 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     min-height: 100vh;
   }
-
 `
 
 export default GlobalStyles
