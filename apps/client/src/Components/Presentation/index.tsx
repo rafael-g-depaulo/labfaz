f9ef35fb90549f5f082930eb7ee85f7f56b8958a
priscila-eng <priscilaangell123@gmail.com>
2021-06-23 17:28:15 -0300

    🐛 Fix react-youtube install

--- apps/client/src/Components/Presentation/index.tsx
+++ apps/client/src/Components/Presentation/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { FC } from 'react'
 import { Container, TextBlock } from './style'
 
@@ -6,17 +7,14 @@ import Loading from "Components/Loading"
 import { useHomePresentationInfo } from 'Api/Presentation';
 import useWidth from 'Hooks/useWidth';
 
-
 export const Presentation: FC = () => {
   const { data, error, isLoading } = useHomePresentationInfo()
-  const { Title, SubTitle, Video} = data!
-
   let width = useWidth();
 
+  if (error) return <div>error: { error?.message ?? "" }</div>
   if (isLoading) return <Loading />
 
-  if (error) return <div>error!</div>
-  console.log(data)
+  const { Title, SubTitle, Video} = data!
   return (
     <Container>
       <div className="ContainerContent">
