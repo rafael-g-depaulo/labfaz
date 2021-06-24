e664e2a416fe6c9acd1ddec5802d852d4130eddd
priscila-eng <priscilaangell123@gmail.com>
2021-06-24 16:40:18 -0300

    💄 Refactor style

--- apps/client/src/Components/Presentation/style.ts
+++ apps/client/src/Components/Presentation/style.ts
@@ -3,52 +3,40 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   justify-content: center;
+  height: 17rem;
 
   .ContainerContent {
     display: flex;
-    justify-content: space-around;
-    width: 100%;
-
-    @media(min-width: 905px) and (max-width: 1250px) {
-      width: 90%;
-    }
-
-    @media(min-width: 1250px) {
-      width: 80%;
-    }
-
-    @media(min-width: 1920px) {
-      width: 55%;
-    }
+    justify-content: space-evenly;
+    gap: 3rem;
+    max-width: 60rem;
   }
-
-  
 `
 
 export const TextBlock = styled.div`
-  width: 30rem;
   text-align: left;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: space-between;
-  
-  h1 {
-    font-size: 30px;
-    margin: 0;
-  }
-  p {
-    margin: 0;
-    font-size: 14px;
-  }
-
-  @media(min-width: 768px ) and (max-width: 1080px) {
-    width: 20rem;
-    h1 {
-      font-size: 20px;
-    }
-    p {
-      font-size: 12px;
-    }
-  }
 `
+
+export const TextTitle = styled.h1`
+  font-size: 30px;
+  margin: 0;
+`
+
+export const Subtitle = styled.p`
+  margin: 0;
+  font-size: 14px;
+  text-align: justify;
+`
+
+export const YoutubePlayerResponsive = styled.div`
+  display: flex;
+  align-items: end;
+  div {
+    height: 15rem;
+    width: 30rem;
+  }
+`;
