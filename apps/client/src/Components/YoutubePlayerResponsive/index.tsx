31fb3d9ec57e6551682e5c14011e3446f573af6e
priscila-eng <priscilaangell123@gmail.com>
2021-06-24 16:43:17 -0300

    💄 Refactor Resposiveness of Youtube Player

--- apps/client/src/Components/YoutubePlayerResponsive/index.tsx
+++ apps/client/src/Components/YoutubePlayerResponsive/index.tsx
@@ -1,40 +1,9 @@
 import React, { FC } from 'react'
 
-import { YoutubePlayer } from 'Components/YoutubePlayer'
 
+export const YoutubePlayerResponsive: FC = () => {
 
-export interface YoutubePlayerResponsiveProps {
-  width: number,
-  href: string,
-}
-
-export const YoutubePlayerResponsive: FC<YoutubePlayerResponsiveProps> = ({
-  width, href
-}) => {
-
-  if(width >= 768 && width <= 844) {
-    return (
-      <YoutubePlayer
-        href={href}
-        width={400}
-        height={200}
-      /> )
-  } else if (width > 844 && width <= 1080) {
-    return (
-      <YoutubePlayer
-        href={href}
-        width={500}
-        height={300}
-      />
-    )
-  } else {
-    return (
-      <YoutubePlayer
-        href={href}
-        width={500}
-      />
-    )
-  }
+  return <div></div>
 }
 
 export default YoutubePlayerResponsive
