c65d2ce498a56e7bbbb08148bc61349b74135e23
priscila-eng <priscilaangell123@gmail.com>
2021-06-24 17:05:13 -0300

    Revert "Merge remote-tracking branch 'origin/feature/apresentation#17' into feature/apresentation#17"
    This reverts commit b289aa7d4be758bfbb404a7b2ad4f63ad6879ef6, reversing
    changes made to 31fb3d9ec57e6551682e5c14011e3446f573af6e.

--- apps/client/src/Components/Presentation/index.tsx
+++ apps/client/src/Components/Presentation/index.tsx
@@ -1,38 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { FC } from 'react'
-import {
-  Container,
-  TextBlock,
-  TextTitle,
-  Subtitle,
-  YoutubePlayerResponsive
-} from './style'
+import { Container, TextBlock } from './style'
 
-// import { YoutubePlayerResponsive } from 'Components/YoutubePlayerResponsive'
-import { YoutubePlayer } from 'Components/YoutubePlayer'
-// import Loading from "Components/Loading"
-// import { useHomePresentationInfo } from 'Api/Presentation';
-// 
+import { YoutubePlayerResponsive } from 'Components/YoutubePlayerResponsive'
+import Loading from "Components/Loading"
+import { useHomePresentationInfo } from 'Api/Presentation';
+import useWidth from 'Hooks/useWidth';
 
 export const Presentation: FC = () => {
-  // const { data, error, isLoading } = useHomePresentationInfo()
-  // if (error) return <div>error: { error?.message ?? "" }</div>
-  // if (isLoading) return <Loading />
+  const { data, error, isLoading } = useHomePresentationInfo()
+  let width = useWidth();
 
-  // const { Title, SubTitle, Video} = data!
+  if (error) return <div>error: { error?.message ?? "" }</div>
+  if (isLoading) return <Loading />
 
-  const Title = 'LABFAZ'
-  const SubTitle = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.'
-  const Video = 'https://www.youtube.com/watch?v=VriRYkpVVMc'
-
+  const { Title, SubTitle, Video} = data!
   return (
     <Container>
       <div className="ContainerContent">
-        <YoutubePlayerResponsive>
-          <YoutubePlayer href={Video} />
-        </YoutubePlayerResponsive>
+        <YoutubePlayerResponsive href={Video} width={width}/>
         <TextBlock>
-          <TextTitle> {Title} </TextTitle>
-          <Subtitle> {SubTitle} </Subtitle>
+          <h1> {Title} </h1>
+          <p> {SubTitle} </p>
         </TextBlock>
       </div>
     </Container>
