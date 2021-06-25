242f5a4152f1c57f0332017cf4e87f31191f8635
priscila-eng <priscilaangell123@gmail.com>
2021-06-25 15:16:51 -0300

    💄 Add display in storybook

--- apps/client/src/Components/Presentation/Presentation.stories.tsx
+++ apps/client/src/Components/Presentation/Presentation.stories.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import Presentation from './'
+import Display from './Display'
 
 storiesOf("Components/Presentation", module)
   .addParameters({ component: Presentation })
   .add("presentation", () => <Presentation />)
+  .add("presentation", () =>
+  <Display
+    Title="LABFAZ"
+    SubTitle="Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+      Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies
+      nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."
+    Video="https://www.youtube.com/watch?v=5qap5aO4i9A"
+  />)
