f890f2acd39d10e2159570fa05d8ce43f8c9a95e
priscila-eng <priscilaangell123@gmail.com>
2021-06-25 20:48:56 -0300

    ✨ Add new breakpoints

--- apps/client/src/Utils/breakpoints/webOnly.ts
+++ apps/client/src/Utils/breakpoints/webOnly.ts
@@ -1,5 +1,6 @@
 import { css, CSSProp } from "styled-components"
 
+
 export const ShortScreen = (cssCode: CSSProp) => css`
   @media (max-width: 780px) {
     ${cssCode}
@@ -14,6 +15,27 @@ export const MediumScreen = (cssCode: CSSProp) => css`
 
 export const LargeScreen = (cssCode: CSSProp) => css`
   @media (min-width: 881px) and (max-width: 1280px) {
+    ${cssCode}
+  }
+`
+export const PlayerSmallerWebScreen = (cssCode: CSSProp) => css`
+  @media(min-width: 530px) and (max-width: 855px) {
+    ${cssCode}
+  }
+`
+
+export const PlayerShortWebScreen = (cssCode: CSSProp) => css`
+  @media (max-width: 855px) {
+    ${cssCode}
+  }
+`
+export const PlayerMediumWebScreen = (cssCode: CSSProp) => css`
+  @media (min-width: 856px) and (max-width: 959px) {
+    ${cssCode}
+  }
+`
+export const PlayerLargeWebScreen = (cssCode: CSSProp) => css`
+  @media (min-width: 960px) and (max-width: 1150px) {
     ${cssCode}
   }
 `
