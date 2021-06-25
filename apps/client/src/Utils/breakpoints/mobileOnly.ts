a7a4ca1992994abe8826fcd85a7698b1be27f32d
priscila-eng <priscilaangell123@gmail.com>
2021-06-25 20:48:56 -0300

    ✨ Add new breakpoints

--- apps/client/src/Utils/breakpoints/mobileOnly.ts
+++ apps/client/src/Utils/breakpoints/mobileOnly.ts
@@ -1,7 +1,29 @@
 import { css, CSSProp } from "styled-components"
 
-export default (cssCode: CSSProp) => css`
-  @media (max-width: 1023px) {
+export const MobileOnly = (cssCode: CSSProp) => css`
+  @media (max-width: 500px) {
+    ${cssCode}
+  }
+`
+export const ScreenMobileSmaller = (cssCode: CSSProp) => css`
+  @media(max-width: 330px) {
+    ${cssCode}
+  }
+`
+export const ScreenMobileShort = (cssCode: CSSProp) => css`
+  @media(min-width: 375px) and (max-width: 459px) {
+    ${cssCode}
+  }
+`
+
+export const ScreenMobileMedium = (cssCode: CSSProp) => css`
+  @media(min-width: 460px) and (max-width: 529px) {
+    ${cssCode}
+  }
+`
+
+export const ScreenMobileLarge = (cssCode: CSSProp) => css`
+  @media(max-width: 374px) {
     ${cssCode}
   }
 `
