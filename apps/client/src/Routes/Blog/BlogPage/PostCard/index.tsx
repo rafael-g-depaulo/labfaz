2b80ce5b3054c92a5543623513e87efca1a89f76
clarice42 <claramaia820@gmail.com>
2021-06-26 20:35:04 -0300

    📱 Add mobile layout

--- apps/client/src/Routes/Blog/BlogPage/PostCard/index.tsx
+++ apps/client/src/Routes/Blog/BlogPage/PostCard/index.tsx
@@ -8,40 +8,72 @@ import {
   Image,
   Button,
   ButtonText,
+  ButtonWrapper,
   DateContainer,
   Date,
   Divisor,
 } from "./styles";
 
 import Test from "./perfil.jpg";
+import useMobile from "Utils/useMobile";
 
 export const PostCard: FC = () => {
+  const isMobile = useMobile();
+
   return (
     <Fragment>
       <Container>
-        <Content>
-          <MainTextContainer>
-            <Title>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
-            </Title>
-            <Description>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
-              sem. Nulla consequat massa quis enim. Donec.
-            </Description>
-            <Image src={Test} alt="example" />
-            <Button>
-              <ButtonText>leia mais</ButtonText>
-            </Button>
-          </MainTextContainer>
-          <Divisor />
-          <DateContainer>
-            <Date>Segunda</Date>
-            <Date>14:00</Date>
-          </DateContainer>
-        </Content>
+        {isMobile ? (
+          <Content>
+            <MainTextContainer>
+              <Title>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              </Title>
+              <Description>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                quis, sem. Nulla consequat massa quis enim. Donec.
+              </Description>
+              <Image src={Test} alt="example" />
+            </MainTextContainer>
+            <ButtonWrapper>
+              <Button>
+                <ButtonText>leia mais</ButtonText>
+              </Button>
+              <Divisor />
+              <DateContainer>
+                <Date>Segunda</Date>
+                <Date>14:00</Date>
+              </DateContainer>
+            </ButtonWrapper>
+          </Content>
+        ) : (
+          <Content>
+            <MainTextContainer>
+              <Title>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              </Title>
+              <Description>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                quis, sem. Nulla consequat massa quis enim. Donec.
+              </Description>
+              <Image src={Test} alt="example" />
+              <Button>
+                <ButtonText>leia mais</ButtonText>
+              </Button>
+            </MainTextContainer>
+            <Divisor />
+            <DateContainer>
+              <Date>Segunda</Date>
+              <Date>14:00</Date>
+            </DateContainer>
+          </Content>
+        )}
       </Container>
     </Fragment>
   );
--- apps/client/src/Routes/Blog/BlogPage/PostCard/styles.ts
+++ apps/client/src/Routes/Blog/BlogPage/PostCard/styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from "styled-components";
 import mobileOnly from "Utils/breakpoints/mobileOnly";
 
+
 export const Container = styled.div`
-  //height: 100%;
   display: grid;
   width: 100%;
   grid-template-columns: minmax(5%, auto) minmax(450px, 800px) minmax(5%, auto);
@@ -10,16 +10,13 @@ export const Container = styled.div`
   grid-template-areas:
     ". content ."
     ".    .    .";
-  /* ${mobileOnly(css`
-    grid-template-columns: minmax(150px, 300px);
-    grid-template-rows: minmax(200px, 400px) minmax(200px, 400px);
-    grid-template-areas:
-      "CardContainer"
-      "CardContainer";
-    margin: 30px 0;
-    justify-items: center;
-    align-items: center;
-  `)} */
+
+  ${mobileOnly(css`
+    grid-template-columns: minmax(8%, auto) minmax(250px, 700px) minmax(
+        8%,
+        auto
+      );
+  `)}
 `;
 
 export const Content = styled.div`
@@ -30,6 +27,12 @@ export const Content = styled.div`
   justify-content: center;
   align-items: flex-start;
   margin: 100px 0;
+
+  ${mobileOnly(css`
+    flex-direction: column;
+    align-items: center;
+    margin: 70px 0;
+  `)}
 `;
 
 export const MainTextContainer = styled.div`
@@ -41,7 +44,6 @@ export const MainTextContainer = styled.div`
 `;
 
 export const Title = styled.h1`
-  //height: 80px;
   font-size: 35px;
   font-weight: bold;
   text-align: left;
@@ -51,14 +53,26 @@ export const Title = styled.h1`
   display: -webkit-box;
   -webkit-line-clamp: 2; /* number of lines to show */
   -webkit-box-orient: vertical;
+
+  ${mobileOnly(css`
+    font-size: 24px;
+    margin: 0;
+    line-height: 1.5;
+  `)}
 `;
 
 export const Description = styled.p`
   font-size: 20px;
-  //font-weight: bold;
   text-align: left;
   line-height: 1.5;
   margin: 50px 0 40px 0;
+
+  ${mobileOnly(css`
+    font-size: 14px;
+    margin: 35px 0;
+    line-height: 2;
+    text-align: justify;
+  `)}
 `;
 
 export const Image = styled.img`
@@ -67,6 +81,19 @@ export const Image = styled.img`
   object-fit: cover;
   object-position: center;
   margin: 0 0 80px 0;
+
+  ${mobileOnly(css`
+    height: 295px;
+    margin: 0 0 40px 0;
+  `)}
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
 `;
 
 export const Button = styled.button`
@@ -78,7 +105,11 @@ export const Button = styled.button`
   justify-content: center;
   outline: none;
   border: none;
-  //margin: 80px 0 0 0;
+
+  ${mobileOnly(css`
+    width: 150px;
+    height: 35px;
+  `)}
 `;
 
 export const ButtonText = styled.p`
@@ -86,11 +117,20 @@ export const ButtonText = styled.p`
   font-weight: 700;
   text-transform: uppercase;
   margin: 0;
+
+  ${mobileOnly(css`
+    font-size: 10px;
+  `)}
 `;
 
 export const Divisor = styled.hr`
   height: 70px;
   margin: 5px 0;
+
+  ${mobileOnly(css`
+    height: 35px;
+    margin: 0;
+  `)}
 `;
 
 export const DateContainer = styled.div`
@@ -100,6 +140,11 @@ export const DateContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   margin-left: 25px;
+
+  ${mobileOnly(css`
+    width: auto;
+    margin: 0;
+  `)}
 `;
 
 export const Date = styled.p`
@@ -108,4 +153,9 @@ export const Date = styled.p`
   font-weight: 500;
   text-align: center;
   margin: 0 0 10px 0;
+
+  ${mobileOnly(css`
+    font-size: 14px;
+    margin: 0 0 5px 0;
+  `)}
 `;
