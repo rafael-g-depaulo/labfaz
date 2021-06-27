f94bc5f5cac661d55f7b2083a7f11a8c6ecf7a8b
priscila-eng <priscilaangell123@gmail.com>
2021-06-27 20:26:06 -0300

    ✅ Add tests for Blog Page

---
+++ apps/client/src/Routes/Blog/BlogPage/Display.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import render from "Utils/render"
+import BlogPageDisplay from "./Display"
+import { BlogBannerInfo } from 'Api/BlogBannerInfo'
+
+describe("Blog Page", () => {
+  it("renders without exploding", () => {
+    const mockedData: BlogBannerInfo = {
+      title: "Blog",
+      subtitle: "LoremIpsum",
+      image: {
+        url: "https://labfaz-strapi-assets.s3.sa-east-1.amazonaws.com/Whats_App_Image_2020_12_19_at_17_23_28_439c4529a0.jpeg",
+        alternativeText: "Blog Banner Image",
+        caption: "string",
+        width: 20,
+        height: 20,
+        ext: "jpeg"
+      }
+    }
+    expect(() => render(<BrowserRouter><BlogPageDisplay data={mockedData} /></BrowserRouter>)).not.toThrow()
+  })
+
+  it("displays the data message", () => {
+    const mockedData: BlogBannerInfo = {
+      title: "Blog",
+      subtitle: "LoremIpsum",
+      image: {
+        url: "https://labfaz-strapi-assets.s3.sa-east-1.amazonaws.com/Whats_App_Image_2020_12_19_at_17_23_28_439c4529a0.jpeg",
+        alternativeText: "Blog Banner Image",
+        caption: "string",
+        width: 20,
+        height: 20,
+        ext: "jpeg"
+      }
+    }
+    const { getByRole } = render(<BrowserRouter><BlogPageDisplay data={mockedData} /></BrowserRouter>)
+
+    expect(getByRole("heading", {level: 1})).toHaveTextContent("Blog")
+    expect(getByRole("heading", {level: 2})).toHaveTextContent("LoremIpsum")
+  })
+})
