b6b3530a23d5888607ff00936d734697c92a2d5c
zipudhe <lucasramalho29@gmail.com>
2021-06-29 17:44:14 -0300

    ✨ initiliaze analytics on app

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import React, { useEffect } from 'react'
 
 import GlobalStyles from 'GlobalStyles'
 import Routes from 'Routes'
 import { showRoutes } from 'FeatureFlags'
 import Contruction from 'Pages/Contruction'
 
+import ReactGA from 'react-ga';
+
+
 export const App = () => {
+  useEffect(() => {
+    ReactGA.initialize('UA-200607957-1')
+
+    ReactGA.pageview(window.location.pathname)
+  })
+
   return (
     <>
       <GlobalStyles />
