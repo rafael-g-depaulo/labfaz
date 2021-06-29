5b5cceacc82eaebe2410f05917c8eb59c8bea403
zipudhe <lucasramalho29@gmail.com>
2021-06-29 10:12:54 -0300

    ♻️ Update welcome with correct data and image type

--- apps/client/src/Routes/AboutUs/AboutUsPage/WelcomeComponent/WelcomeComponent.stories.tsx
+++ apps/client/src/Routes/AboutUs/AboutUsPage/WelcomeComponent/WelcomeComponent.stories.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import WelcomeComponent from "./"
+import { DataObject } from 'Api/AboutUs'
+import { mockImage } from 'Utils/Image'
 
-const welcome_data = {
+const image = mockImage({
+  url: "https://st.depositphotos.com/1002049/2024/i/950/depositphotos_20240775-stock-photo-welcome.jpg",
+  alternativeText: "What can I say except you're welcome",
+  caption: "A very nice welcome image",
+  ext: 'jpg'
+})
+
+const welcome_data: DataObject = {
   id: 1,
-  img: {
-    name: "Welcome image",
-    url: "https://st.depositphotos.com/1002049/2024/i/950/depositphotos_20240775-stock-photo-welcome.jpg",
-    img: "https://st.depositphotos.com/1002049/2024/i/950/depositphotos_20240775-stock-photo-welcome.jpg"
-  },
+  img: image,
   text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus."
 }
 
