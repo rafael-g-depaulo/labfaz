94f7f02d0502cd0ef63cc94d5d293636a790977e
priscila-eng <priscilaangell123@gmail.com>
2021-07-01 16:29:33 -0300

    💄 Fix margin in logo

--- apps/client/src/Components/Logo/style.ts
+++ apps/client/src/Components/Logo/style.ts
@@ -4,6 +4,7 @@ import {DesktopSmall} from 'Utils/breakpoints'
 export const Title = styled.h1`
   font-size: var(--font-size-title-small);
   color: var(--background-pink);
+  margin: 0;
 
   ${DesktopSmall(css`
     font-size: 22px;
