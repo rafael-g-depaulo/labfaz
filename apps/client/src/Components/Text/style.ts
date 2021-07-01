fc91f2a3b63a64f5080eb3d30a2d0b0bbbc34fb6
zipudhe <lucasramalho29@gmail.com>
2021-07-01 19:36:09 -0300

    💄 Add properties to Text component

--- apps/client/src/Components/Text/style.ts
+++ apps/client/src/Components/Text/style.ts
@@ -5,22 +5,23 @@ export interface ParagraphProps {
   textAlign?: string
   weight?: string
   marginTop?: string
+  marginBottom?: string
   fontSize?: string
   color?: string
+  fontWeight?: number
 }
 
 
 export const Paragraph = styled.p<ParagraphProps>`
   font-style: normal;
   margin-top: 0;
-  font-size: var(--font-size-default);
   text-align: ${props => props.textAlign ? props.textAlign : "left"};
   font-size: ${props => props.fontSize ? 
   "var(--font-size-"+ props.fontSize + ")" 
   :
   "var(--font-size-default)"
   };
-
+  font-weight: ${props => props.fontWeight ? props.fontWeight : 400};
 
   ${
     Mobile(css`
