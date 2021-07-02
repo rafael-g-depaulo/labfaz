b4d2f0ba7f27ee2bf74ced100d3035127f41dca6
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-01 23:51:26 -0300

    ✨ feature: Add styles for Global Button Component
    In this commit will add variant of
    buttons styles.

--- apps/client/src/Components/Buttons/ExternalLinkButton/style.ts
+++ apps/client/src/Components/Buttons/ExternalLinkButton/style.ts
@@ -1,24 +1,37 @@
-// import styled from 'styled-components'
-// import { Link } from 'react-router-dom'
+import styled, { css } from 'styled-components'
+import { Link } from 'react-router-dom'
+import { ButtonHTMLAttributes } from 'react';
 
-// import { Mobile } from 'Utils/breakpoints'
+interface ButtonProps {
+  buttonColor?: 'white' | 'gray' 
+}
 
-// // TODO import image from 'Assets/image.jpg'
+const ButtonVariation = {
 
-// export interface Props {
-//   align: string
-// }
+  'white': css`  
+    background-color: var(--background-white);
+    cursor: pointer;
+    /* TODO border-radius: ; */
+    padding: 0.5rem 2rem;
+    text-decoration: none;
+    /* TODO color: ; */
+    color: var(--color-text);
+    border: 0;
+    /* $ {ShortScreen('font-size: var(--font-size-short); padding: 0.5rem 1rem')}; */
+    /* $ {MediumScreen('font-size: var(--font-size-medium)')}; */
+    /* $ {LargeScreen('font-size: var(--font-size-large)')}; */
+  `,
 
-// export const StyledButton = styled.div`
-//    /* TODO */
-//     background-color: var(--background-white);
-//     cursor: pointer;
-//     /* TODO border-radius: ; */
-//     padding: 0.5rem 2rem;
-//     text-decoration: none;
-//     /* TODO color: ; */
-//     color: var(--color-text);
-//     /* $ {ShortScreen('font-size: var(--font-size-short); padding: 0.5rem 1rem')}; */
-//     /* $ {MediumScreen('font-size: var(--font-size-medium)')}; */
-//     /* $ {LargeScreen('font-size: var(--font-size-large)')}; */
-// `;
+  'gray': css`
+    text-decoration: none;
+    color: var(--color-text);
+    padding: 0.5rem 3rem;
+    background-color: var(--background-dark-gray);
+    border: 0;
+    cursor: pointer;
+  `,
+};
+
+export const StyledButton = styled.button<ButtonProps>`
+  ${(props) => ButtonVariation[props.buttonColor || 'white' ]}
+`
