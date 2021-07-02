03c9c5222b3ea3a1080e70a4d52edf86f27f15a9
priscila-eng <priscilaangell123@gmail.com>
2021-07-02 08:14:34 -0300

    🐛 Fix test in Logo Component

--- apps/client/src/Components/Logo/index.test.tsx
+++ apps/client/src/Components/Logo/index.test.tsx
@@ -3,11 +3,11 @@ import render from "Utils/render"
 
 import Logo from './'
 
-it("renders Partners component", () => {
+it("renders Logo component", () => {
   expect(() => render(<Logo />)).not.toThrow()
 })
 
 it("check if renders tag img", () => {
   const { getByRole } = render(<Logo />)
-  expect(getByRole('img')).toHaveAttribute('alt', 'logo')
+  expect(getByRole('heading', { level: 1})).toHaveTextContent('LABFAZ')
 })
