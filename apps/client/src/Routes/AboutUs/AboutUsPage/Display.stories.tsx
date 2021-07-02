d767e1a9a2524ebb2ee22b125e4f21ca7155e829
zipudhe <lucasramalho29@gmail.com>
2021-07-01 21:30:32 -0300

    🎨 💄 Add Staff to about us page

--- apps/client/src/Routes/AboutUs/AboutUsPage/Display.stories.tsx
+++ apps/client/src/Routes/AboutUs/AboutUsPage/Display.stories.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import AboutUsPage from './Display'
 import { mockImage } from 'Utils/Image'
-import { AboutUsData } from 'Api/AboutUs'
+import { AboutUsData, StaffData, StaffObject } from 'Api/AboutUs'
 
 const image = mockImage({
   url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsegredosdomundo.r7.com%2Fwp-content%2Fuploads%2F2019%2F10%2Fornitorrinco-principais-caracteristicas-10-curiosidades-4.jpg&f=1&nofb=1",
@@ -15,27 +15,45 @@ const image = mockImage({
   ext: "png"
 })
 
-const mockedData: AboutUsData = {
+const Staffimage = mockImage({
+  url: "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  alternativeText: "Descriptive of person",
+  caption: "A nice caption",
+  ext: "jpg"
+})
+
+const StaffInfo: StaffObject = {
+  name: "Jhon Doe",
+  tag: "Coordenação",
+  text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
+  image: Staffimage
+}
+
+const mockedAboutUsData: AboutUsData = {
   banner_data: "Quem Somos",
   welcome_data: {
     id: 1,
     img: image,
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ",
   },
   about_us_data: [{
     id: 2,
     img: image,
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ",
 
   },
   {
     id: 2,
     img: image,
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ",
 
   }]
 }
 
+const mockedStaffData: StaffData = {
+  staffs: [StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo, StaffInfo]
+}
+
 storiesOf("Pages/AboutUs", module)
   .addParameters({ component: AboutUsPage })
-  .add("default", () => <BrowserRouter><AboutUsPage data={mockedData}/></BrowserRouter>)
+  .add("default", () => <BrowserRouter><AboutUsPage data={mockedAboutUsData} staff={mockedStaffData}/></BrowserRouter>)
--- apps/client/src/Routes/AboutUs/AboutUsPage/Display.tsx
+++ apps/client/src/Routes/AboutUs/AboutUsPage/Display.tsx
@@ -1,4 +1,4 @@
-import { AboutUsData } from "Api/AboutUs"
+import { AboutUsData, StaffData } from "Api/AboutUs"
 import React, { FC } from "react"
 
 import Fullpage from "Components/FullPage"
@@ -6,21 +6,27 @@ import Header from "Components/Header"
 import Banner from "Components/Banner"
 import WelcomeComponent from "./WelcomeComponent"
 import About from "./AboutUsComponent"
+import Staff from "./StaffComponent"
 
 export interface DisplayProps {
   data: AboutUsData
+  staff: StaffData
 }
 
 export const Display: FC<DisplayProps> = ({
   data,
+  staff
 }) => {
   const {banner_data, welcome_data, about_us_data} = data!
+  const staff_data = staff.staffs
+
   return (
     <Fullpage>
       <Header />
       <Banner title="Quem somos" subtitle={banner_data} align="left"/>
       <WelcomeComponent data={welcome_data} />
       <About  data={about_us_data} />
+      <Staff  data={staff_data}/>
     </Fullpage>
   )
 }
