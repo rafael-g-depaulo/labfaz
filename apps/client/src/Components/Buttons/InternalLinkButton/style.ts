de48047badeda5fc1ecc3d06db06f978242878cc
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-01 23:45:34 -0300

    ✨ feature: Add styles for Global Button Component
    In this commit will add variant of
    buttons styles.

---
+++ apps/client/src/Components/Buttons/InternalLinkButton/style.ts
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components'
+import { Link } from 'react-router-dom'
+import { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps {
+  buttonColor?: 'white' | 'gray' 
+}
+
+type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>
+
+const ButtonVariation = {
+
+  'white': css`  
+    background-color: var(--background-white);
+    cursor: pointer;
+    /* TODO border-radius: ; */
+    padding: 0.5rem 2rem;
+    text-decoration: none;
+    /* TODO color: ; */
+    color: var(--color-text);
+    border: 0;
+    /* $ {ShortScreen('font-size: var(--font-size-short); padding: 0.5rem 1rem')}; */
+    /* $ {MediumScreen('font-size: var(--font-size-medium)')}; */
+    /* $ {LargeScreen('font-size: var(--font-size-large)')}; */
+  `,
+
+  'gray': css`
+    text-decoration: none;
+    color: var(--color-text);
+    padding: 0.5rem 3rem;
+    background-color: var(--background-dark-gray);
+    border: 0;
+    cursor: pointer;
+  `,
+};
+
+export const StyledButton = styled(Link)<ButtonProps & ButtonType>`
+  ${(props) => ButtonVariation[props.buttonColor || 'white' ]}
+`
