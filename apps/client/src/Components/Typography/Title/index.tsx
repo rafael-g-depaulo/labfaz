0734b959feac370f88f63625751dc4442570c3eb
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-01 23:41:09 -0300

    ✨ feature: Create global text component
    In this commit will add different levels of
    fontsizes for text.

--- apps/client/src/Components/Typography/Title/index.tsx
+++ apps/client/src/Components/Typography/Title/index.tsx
@@ -4,16 +4,16 @@ import { StyledTitle } from './style'
 
 export interface TitleProps {
   level?: 0 | 1 | 2 | 3 | 4 
-  as?: string
 }
 
 export const Title: FC<TitleProps> = ({
   level = 1,
-  children
+  children,
+  ...props
 }) => {
 
   return (
-    <StyledTitle level={level}>
+    <StyledTitle level={level} {...props}>
       {children}
     </StyledTitle>
   )
