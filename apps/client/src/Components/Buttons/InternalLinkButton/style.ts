f3f481c9aed621c80c0df3862871cf94d69bdfd2
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-03 18:41:48 -0300

    ✨feature: add primary and secondary colors to buttons

--- apps/client/src/Components/Buttons/InternalLinkButton/style.ts
+++ apps/client/src/Components/Buttons/InternalLinkButton/style.ts
@@ -3,12 +3,49 @@ import { Link } from 'react-router-dom'
 import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps {
-  buttonColor?: 'white' | 'gray' 
+  buttonColor?: 'primary' | 'secondary' | 'secondary-variant-1' | 'white' | 'gray'
 }
 
 type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>
 
 const ButtonVariation = {
+
+  'primary': css`
+    background-color: var(--button-color-pink);
+    cursor: pointer;
+    /* TODO border-radius: ; */
+    padding: 0.5rem 2rem;
+    text-decoration: none;
+    /* TODO color: ; */
+    color: var(--color-text-yellow);
+    border-top: 2px solid var(--button-color-yellow);
+    border-left: 2px solid var(--button-color-yellow);
+    /* $ {ShortScreen('font-size: var(--font-size-short); padding: 0.5rem 1rem')}; */
+    /* $ {MediumScreen('font-size: var(--font-size-medium)')}; */
+    /* $ {LargeScreen('font-size: var(--font-size-large)')}; */
+  `,
+
+  'secondary': css`
+    text-decoration: none;
+    color: var(--color-text-pink);
+    padding: 0.5rem 3rem;
+    background-color: var(--button-color-yellow);
+    border: 0;
+    cursor: pointer;
+    border-top: 2px solid black;
+    border-left: 2px solid black;
+  `,
+
+  'secondary-variant-1': css`
+    text-decoration: none;
+    color: var(--color-text-pink);
+    padding: 0.5rem 3rem;
+    background-color: var(--button-color-yellow);
+    border: 0;
+    cursor: pointer;
+    border-top: 2px solid var(--button-color-pink);
+    border-left: 2px solid var(--button-color-pink);
+  `,
 
   'white': css`  
     background-color: var(--background-white);
