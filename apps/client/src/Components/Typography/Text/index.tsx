95e55ca463ce02a714745ecdbae60df2aa15e27d
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-03 19:28:10 -0300

    ✨ feature: add colors variation to text and title props

--- apps/client/src/Components/Typography/Text/index.tsx
+++ apps/client/src/Components/Typography/Text/index.tsx
@@ -4,16 +4,22 @@ import { StyledText } from './style'
 
 export interface TitleProps {
   level?: 0 | 1 | 2 | 3 | 4 
+  color?: 'white' | 'yellow' | 'black' | 'pink' 
 }
 
 export const Text: FC<TitleProps> = ({
   level = 0,
   children,
+  color,
   ...props
 }) => {
 
   return (
-    <StyledText level={level} {...props}>
+    <StyledText 
+      level={level} 
+      color={color}
+      {...props}
+    >
       {children}
     </StyledText>
   )
--- apps/client/src/Components/Typography/Title/index.tsx
+++ apps/client/src/Components/Typography/Title/index.tsx
@@ -4,16 +4,22 @@ import { StyledTitle } from './style'
 
 export interface TitleProps {
   level?: 0 | 1 | 2 | 3 | 4 
+  color?: 'white' | 'yellow' | 'black' | 'pink' 
 }
 
 export const Title: FC<TitleProps> = ({
   level = 1,
   children,
+  color,
   ...props
 }) => {
 
   return (
-    <StyledTitle level={level} {...props}>
+    <StyledTitle 
+      level={level} 
+      color={color}
+      {...props}
+    >
       {children}
     </StyledTitle>
   )
