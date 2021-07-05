987ae79c971aac8aa9e807dfaf75fe5db720c2cb
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-05 02:26:29 -0300

    ✨ feature: add stories for Text component

---
+++ apps/client/src/Components/Typography/Text/Text.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Text } from './'
+
+storiesOf("Components/Text", module)
+  .addParameters({ component: Text })
+  .add("text", () => 
+    <BrowserRouter >
+      <Text 
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+        porta ligula nibh, nec interdum nunc maximus at."
+        textColor="black"
+      />
+    </BrowserRouter>)
