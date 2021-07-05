0f403a4ac0911671eb5b06ed1038ad5e0bcdafe4
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-05 03:00:37 -0300

    ♻ fix: refactor style logic of Title Component

--- apps/client/src/Components/Typography/Title/style.ts
+++ apps/client/src/Components/Typography/Title/style.ts
@@ -1,53 +1,48 @@
 import styled, { css } from 'styled-components'
-
 interface TitleProps {
-  level?: 0 | 1 | 2 | 3 | 4
-  color?: 'white' | 'yellow' | 'black' | 'pink' 
+  level?: 1 | 2 | 3 | 4 | 5
 }
 
 const TitleFontVariation = {
-
-  0: css`
-    font-size: var(--font-size-default);
-  `,
 
   1: css`
     font-size: var(--font-size-title);
   `,
 
   2: css`
+    font-size: var(--font-size-default);
+  `,
+
+  3: css`
     font-size: var(--font-size-large);
   `,
   
-  3: css`
+  4: css`
     font-size: var(--font-size-medium);
   `,
   
-  4: css`
+  5: css`
     font-size: var(--font-size-short);
   `,
 };
 
-const TitleColorVariation = {
-  'white': css`
-    color: var(--color-text-white);
-  `,
-
-  'yellow': css`
-    color: var(--color-text-yellow);
-  `,
-
-  'black': css`
-    color: var(--color-text);  
-  `,
-
-  'pink': css`
-  color: var(--color-text-pink);  
-  `
-}
-
 export const StyledTitle = styled.h1<TitleProps>`
-  ${(props) => TitleFontVariation[props.level || 0]};
-  ${(props) => TitleColorVariation[props.color || 'black']}
+  ${(props) => TitleFontVariation[props.level || 1]};
   margin: 0;
+  
+  &.white {
+    color: var(--color-text-white);
+  }
+
+  &.yellow {
+    color: var(--color-text-yellow);
+  } 
+
+  &.black {
+    color: var(--color-text);  
+  }
+
+  &.pink {
+    color: var(--color-text-pink);  
+  }
 `
