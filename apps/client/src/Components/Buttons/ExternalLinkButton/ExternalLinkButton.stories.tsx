b331874dbaf2e4a32b7efc207b57e837e5432dfd
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-05 02:25:20 -0300

    ✨ feature: add stories for External link button

---
+++ apps/client/src/Components/Buttons/ExternalLinkButton/ExternalLinkButton.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { ExternalLinkButton } from './'
+
+storiesOf("Components/ExternalLinkButton", module)
+  .addParameters({ component: ExternalLinkButton })
+  .add("externalLinkButton", () => 
+    <BrowserRouter >
+      <ExternalLinkButton 
+        href="https://google.com.br" 
+        buttonColor="primary"
+        children="External Button"
+      />
+    </BrowserRouter>)
