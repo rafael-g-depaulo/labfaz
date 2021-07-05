6651c2bbf7976af719d215f51e21e05a68bc3b8f
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-07-05 20:25:35 -0300

    💩 Temporary fix

--- apps/client/src/Components/CoursesPresentation/Display.tsx
+++ apps/client/src/Components/CoursesPresentation/Display.tsx
@@ -4,7 +4,7 @@ import { useMobile } from "Utils/useMobile";
 import { useCoursePresentations } from "Api/CoursePresentation";
 import Card from "./Card";
 import { Homepage } from "Api/Homepage";
-import Loading from "Components/Loading";
+// import Loading from "Components/Loading";
 
 interface DisplayProps {
   texts: Homepage;
@@ -15,8 +15,8 @@ const Display: FC<DisplayProps> = ({ texts }) => {
 
   const result = useCoursePresentations()
 
-  if (result.isLoading) return <Loading />
-  if (result.error) return <div>error: {result.error.message}</div>
+  // if (result.isLoading) return <Loading />
+  // if (result.error) return <div>error: {result.error.message}</div>
 
   return (
     // Change the layout according to the screen's width
@@ -26,7 +26,7 @@ const Display: FC<DisplayProps> = ({ texts }) => {
           <TextContainer>
             <Title>{texts.course_presentation_title}</Title>
             <Text>{texts.course_presentation_first_text}</Text>
-            <Card courses={result.data} isMobile={isMobile} />
+            <Card courses={result.data!} isMobile={isMobile} />
             <Text>{texts.course_presentation_second_text}</Text>
           </TextContainer>
         </>
@@ -37,7 +37,7 @@ const Display: FC<DisplayProps> = ({ texts }) => {
             <Text>{texts.course_presentation_first_text}</Text>
             <Text>{texts.course_presentation_second_text}</Text>
           </TextContainer>
-          <Card courses={result.data} isMobile={isMobile} />
+          <Card courses={result.data!} isMobile={isMobile} />
         </>
       )}
     </Container>
