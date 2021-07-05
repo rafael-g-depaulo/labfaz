389b39c106188bc04c87c4dfc92cf727bf39835e
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-05 02:59:12 -0300

    ♻ fix: refactor style logic of InternalLinkButton Component

--- apps/client/src/Components/Buttons/InternalLinkButton/style.ts
+++ apps/client/src/Components/Buttons/InternalLinkButton/style.ts
@@ -1,76 +1,38 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { ButtonHTMLAttributes } from 'react';
 
-interface ButtonProps {
-  buttonColor?: 'primary' | 'secondary' | 'secondary-variant-1' | 'white' | 'gray'
-}
+export const StyledButton = styled(Link)`
 
-type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>
+  cursor: pointer;
+  border: 0;
+  padding: 0.5rem 2rem;
+  text-decoration: none;
+  border-top: 2px solid;
+  border-left: 2px solid;
 
-const ButtonVariation = {
-
-  'primary': css`
+  &.primary {
     background-color: var(--button-color-pink);
-    cursor: pointer;
     /* TODO border-radius: ; */
-    padding: 0.5rem 2rem;
-    text-decoration: none;
     /* TODO color: ; */
     color: var(--color-text-yellow);
-    border-top: 2px solid var(--button-color-yellow);
-    border-left: 2px solid var(--button-color-yellow);
+    border-color: var(--button-color-yellow);
     /* $ {ShortScreen('font-size: var(--font-size-short); padding: 0.5rem 1rem')}; */
     /* $ {MediumScreen('font-size: var(--font-size-medium)')}; */
     /* $ {LargeScreen('font-size: var(--font-size-large)')}; */
-  `,
+  }
 
-  'secondary': css`
-    text-decoration: none;
+  &.secondary {
     color: var(--color-text-pink);
-    padding: 0.5rem 3rem;
     background-color: var(--button-color-yellow);
-    border: 0;
-    cursor: pointer;
-    border-top: 2px solid black;
-    border-left: 2px solid black;
-  `,
-
-  'secondary-variant-1': css`
-    text-decoration: none;
-    color: var(--color-text-pink);
+    border-color: black;
     padding: 0.5rem 3rem;
-    background-color: var(--button-color-yellow);
-    border: 0;
-    cursor: pointer;
-    border-top: 2px solid var(--button-color-pink);
-    border-left: 2px solid var(--button-color-pink);
-  `,
 
-  'white': css`  
-    background-color: var(--background-white);
-    cursor: pointer;
-    /* TODO border-radius: ; */
-    padding: 0.5rem 2rem;
-    text-decoration: none;
-    /* TODO color: ; */
-    color: var(--color-text);
-    border: 0;
-    /* $ {ShortScreen('font-size: var(--font-size-short); padding: 0.5rem 1rem')}; */
-    /* $ {MediumScreen('font-size: var(--font-size-medium)')}; */
-    /* $ {LargeScreen('font-size: var(--font-size-large)')}; */
-  `,
 
-  'gray': css`
-    text-decoration: none;
-    color: var(--color-text);
-    padding: 0.5rem 3rem;
-    background-color: var(--background-dark-gray);
-    border: 0;
-    cursor: pointer;
-  `,
-};
-
-export const StyledButton = styled(Link)<ButtonProps & ButtonType>`
-  ${(props) => ButtonVariation[props.buttonColor || 'white' ]}
+    &-1 {
+      color: var(--color-text-pink);
+      background-color: var(--button-color-yellow);
+      padding: 0.5rem 3rem;
+      border-color: var(--button-color-pink);
+    }
+  }
 `
