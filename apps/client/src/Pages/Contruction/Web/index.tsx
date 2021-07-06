4fa2507ca1dc34c946ec82f5f9c8489dc6c23a1a
priscila-eng <priscilaangell123@gmail.com>
2021-07-06 15:35:45 -0300

    ✨ Add central component

--- apps/client/src/Pages/Contruction/Web/index.tsx
+++ apps/client/src/Pages/Contruction/Web/index.tsx
@@ -2,12 +2,33 @@ import React, { FC } from 'react'
 
 import { Header } from '../Header'
 import { Footer } from '../Footer'
-import { Container } from './style'
+import image from '../construction.png'
+import {
+  Container,
+  Text,
+  ImageConstruction,
+  Circle,
+  ImageDiv,
+  Rectangle,
+  Triangle
+} from './style'
 
 export const Web: FC = () => {
   return (
     <Container>
       <Header />
+      <div>
+        <Rectangle> 
+          <Triangle />
+          <ImageDiv>
+            <Circle />
+            <ImageConstruction src={image} alt="construction" />
+          </ImageDiv>
+          <Text>
+            Olá, ainda estamos organizando as coisas.
+          </Text>
+        </Rectangle>
+      </div>
       <Footer />
     </Container>
   )
--- apps/client/src/Pages/Contruction/Web/style.ts
+++ apps/client/src/Pages/Contruction/Web/style.ts
@@ -1,9 +1,58 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+import { DesktopLarge } from 'Utils/breakpoints'
 
 export const Container = styled.div`
   display: flex;
   height: 100vh;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: space-around;
   background-color: var(--background-black);
+  padding: 0 3rem;
+
+  ${ DesktopLarge(css`
+    padding: 0 5rem;
+  `)}
+`;
+
+export const Triangle = styled.div`
+  border-top: 20rem solid transparent;
+  border-left: 16rem solid var(--background-black);
+`;
+
+export const Rectangle = styled.div`
+  background-color: var(--background-pink);
+  height: 20rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 5rem;
+`;
+
+export const Text = styled.h1`
+  max-width: 8.2rem;
+  color: var(--color-text-beige);
+  font-size: var(--font-size-title);
+  margin: 0;
+  line-height: 2.5rem;
+`;
+export const ImageDiv = styled.div`
+  display: flex;
+  position: fixed;
+  left: 9.5rem;
+`;
+
+export const ImageConstruction = styled.img`
+  height: 20rem;
+  width: 20rem;
+  position: fixed;
+  left: 10rem;
+`;
+
+export const Circle = styled.div`
+  height: 20rem;
+  width: 20rem;
+  border-radius: 50%;
+  border: 1px solid var(--background-blue);
+  z-index: 1;
 `;
