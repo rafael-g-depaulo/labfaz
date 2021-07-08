32de1f607c6d282f2e0e66439cb180083486192c
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-08 17:53:22 -0300

    🔥 Remove titleColor of TitleProps and className

--- apps/client/src/Components/Typography/Title/index.tsx
+++ apps/client/src/Components/Typography/Title/index.tsx
@@ -3,22 +3,20 @@ import React, { FC } from 'react'
 import { StyledTitle } from './style'
 
 export interface TitleProps {
-  level?: 1 | 2 | 3 | 4 | 5 
-  titleColor?: 'white' | 'yellow' | 'black' | 'pink' 
+  level?: 1 | 2 | 3 | 4 | 5 | 6
 }
 
 export const Title: FC<TitleProps> = ({
   level = 1,
   children,
-  titleColor,
   ...props
 }) => {
 
   return (
     <StyledTitle 
       as={`h${level}`}
-      className={titleColor}
       children={children}
+      level={level}
       {...props}
     />
   )
