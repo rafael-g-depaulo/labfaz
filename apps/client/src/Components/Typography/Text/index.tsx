fac510d543463306a3bfc29d77659913bbf69bab
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-08 17:48:28 -0300

    🔥 Remove textColor of Text props and className property

--- apps/client/src/Components/Typography/Text/index.tsx
+++ apps/client/src/Components/Typography/Text/index.tsx
@@ -3,18 +3,16 @@ import React, { FC } from 'react'
 import { StyledText } from './style'
 
 export interface TextProps {
-  textColor?: 'white' | 'yellow' | 'black' | 'pink' 
+  children: React.ReactNode
 }
 
 export const Text: FC<TextProps> = ({
   children,
-  textColor,
   ...props
 }) => {
 
   return (
     <StyledText 
-      className={textColor}
       children={children}
       {...props}
     />
