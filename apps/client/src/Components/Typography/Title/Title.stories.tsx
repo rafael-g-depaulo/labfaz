802eb5e1e622c7c80d80cf1a2e2b6f21d8f64ad3
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-08 17:58:17 -0300

    ✨feature: add new components for storybook and remove titleColor property

--- apps/client/src/Components/Typography/Title/Title.stories.tsx
+++ apps/client/src/Components/Typography/Title/Title.stories.tsx
@@ -6,12 +6,57 @@ import { Title } from './'
 
 storiesOf("Components/Title", module)
   .addParameters({ component: Title })
-  .add("title", () => 
+  .add("title h1", () => 
     <BrowserRouter >
       <Title 
         children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
         porta ligula nibh, nec interdum nunc maximus at."
         level={1}
-        titleColor="black"
+      />
+    </BrowserRouter>)
+
+  .add("Title h2", () => 
+    <BrowserRouter >
+      <Title 
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+        porta ligula nibh, nec interdum nunc maximus at."
+        level={2}
+      />
+    </BrowserRouter>)
+
+  .add("Title h3", () => 
+    <BrowserRouter >
+      <Title 
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+        porta ligula nibh, nec interdum nunc maximus at."
+        level={3}
+      />
+    </BrowserRouter>)
+
+  .add("Title h4", () => 
+    <BrowserRouter >
+      <Title 
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+        porta ligula nibh, nec interdum nunc maximus at."
+        level={4}
+      />
+    </BrowserRouter>)
+
+  .add("Title h5", () => 
+    <BrowserRouter >
+      <Title 
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+        porta ligula nibh, nec interdum nunc maximus at."
+        level={5}
+      />
+    </BrowserRouter>)
+
+
+  .add("Title h6", () => 
+    <BrowserRouter >
+      <Title 
+        children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+        porta ligula nibh, nec interdum nunc maximus at."
+        level={6}
       />
     </BrowserRouter>)
