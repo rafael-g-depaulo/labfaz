e2ad673c9ee38e95d6cfac764ddfb02641bd1d2b
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-08 18:08:54 -0300

    🔥 remove styles of InternalLinkButton
    In this commit will remove classes and put a default style for
    InternalLinkButton

--- apps/client/src/Components/Buttons/InternalLinkButton/style.ts
+++ apps/client/src/Components/Buttons/InternalLinkButton/style.ts
@@ -6,33 +6,13 @@ export const StyledButton = styled(Link)`
   cursor: pointer;
   border: 0;
   padding: 0.5rem 2rem;
+  font-weight: bold;
   text-decoration: none;
   border-top: 2px solid;
   border-left: 2px solid;
-
-  &.primary {
-    background-color: var(--button-color-pink);
-    /* TODO border-radius: ; */
-    /* TODO color: ; */
-    color: var(--color-text-yellow);
-    border-color: var(--button-color-yellow);
-    /* $ {ShortScreen('font-size: var(--font-size-short); padding: 0.5rem 1rem')}; */
-    /* $ {MediumScreen('font-size: var(--font-size-medium)')}; */
-    /* $ {LargeScreen('font-size: var(--font-size-large)')}; */
-  }
-
-  &.secondary {
-    color: var(--color-text-pink);
-    background-color: var(--button-color-yellow);
-    border-color: black;
-    padding: 0.5rem 3rem;
-
-
-    &-1 {
-      color: var(--color-text-pink);
-      background-color: var(--button-color-yellow);
-      padding: 0.5rem 3rem;
-      border-color: var(--button-color-pink);
-    }
-  }
+  background-color: var(--button-color-pink);
+  /* TODO border-radius: ; */
+  /* TODO color: ; */
+  color: var(--color-text-yellow);
+  border-color: var(--button-color-yellow);
 `
