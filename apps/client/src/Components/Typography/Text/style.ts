f8382db0790bf3ca0db9514e93a603f5d02bed6e
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-08 17:44:58 -0300

    🔥 remove classes and add a default Text properties

--- apps/client/src/Components/Typography/Text/style.ts
+++ apps/client/src/Components/Typography/Text/style.ts
@@ -2,20 +2,6 @@ import styled from 'styled-components'
 
 export const StyledText = styled.p`
   font-size: var(--font-size-default);
-  
-  &.white {
-    color: var(--color-text-white);
-  }
-
-  &.yellow {
-    color: var(--color-text-yellow);
-  }
-
-  &.black {
-    color: var(--color-text);  
-  }
-
-  &.pink {
-    color: var(--color-text-pink);  
-  }
+  color: var(--color-text-black);
+  margin: 0;
 `
