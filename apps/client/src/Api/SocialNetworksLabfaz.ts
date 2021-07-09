9df995b07d0b5d2671e3e749eb330e73d2ee9904
priscila-eng <priscilaangell123@gmail.com>
2021-07-09 11:24:58 -0300

    ✨ Add instagram

--- apps/client/src/Api/SocialNetworksLabfaz.ts
+++ apps/client/src/Api/SocialNetworksLabfaz.ts
@@ -5,12 +5,13 @@ export interface SocialNetworksLabfaz {
   facebook: string,
   twitter: string,
   googlePlus: string,
-  linkedin: string
+  linkedin: string,
+  instagram: string
 }
 
 export const fetchSocialNetworksLabfaz: () => Promise<SocialNetworksLabfaz> = () => strapi
   .get(`/social-networks-labfaz`)
   .then(({ data }) => data)
-  .then(({ facebook, twitter, googlePlus, linkedin }) => ({ facebook, twitter, googlePlus, linkedin }))
+  .then(({ facebook, twitter, googlePlus, linkedin, instagram }) => ({ facebook, twitter, googlePlus, linkedin, instagram }))
 
 export const useSocialNetworksLabfaz = () => useFetchApi<SocialNetworksLabfaz>(`/social-networks-labfaz`, fetchSocialNetworksLabfaz)
