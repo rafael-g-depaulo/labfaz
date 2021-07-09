ddc11190ac618557129a3ea9bccd9c5d0869c551
zipudhe <lucasramalho29@gmail.com>
2021-07-09 12:25:51 -0300

    ✨ Add email confimation logic

--- apps/api/src/Config/auth.ts
+++ apps/api/src/Config/auth.ts
@@ -3,5 +3,10 @@ export default {
   jwt: {
     secret: '8020877602e2d69c02c2f0377909b8f2',
     expiresIn: '1d'
+  },
+
+  token: {
+    secret: '8020877602e2d69c02c2f0377909b8f2',
+    expiresIn: '600s'
   }
 }
