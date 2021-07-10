9c410034b09178ddb2dfcbe1793c8af7ef135ad6
priscila-eng <priscilaangell123@gmail.com>
2021-07-10 19:10:54 -0300

    🔥 Remove header and banner for home page

--- apps/client/src/Routes/Home/HomePage/index.tsx
+++ apps/client/src/Routes/Home/HomePage/index.tsx
@@ -3,18 +3,13 @@ import React, { FC } from "react"
 import { useHelloExample } from "Api/HelloApiExample"
 import Loading from "Components/Loading"
 import Display from "./Display"
-import Header from 'Components/Header'
-import Banner from 'Components/Banner'
 
 export const HomePage: FC = () => {
 
   const result = useHelloExample()
 
   if (result.isLoading) return <Loading />
-  if (result.error) return <div><Header /> <Banner title="LABFAZ"
-  subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta
-    ligula nibh, nec interdum nunc maximus at."
-  hrefKnowMore="/about"/> error: {result.error.message}</div>
+  if (result.error) return <div>error: {result.error.message}</div>
 
   return (
     <Display data={result.data}/>
