d251354dd1b2e1a238a2c4d5b123871ba83d7a6c
clarice42 <claramaia820@gmail.com>
2021-07-12 14:09:50 -0300

    ✨ Create strapi endpoint

--- apps/client/src/Api/BlogPost.ts
+++ apps/client/src/Api/BlogPost.ts
@@ -1,25 +1,42 @@
-import { strapi } from "Api"
-import useFetchApi from 'Hooks/useFetchApi'
+import { strapi } from "Api";
+import useFetchApi from "Hooks/useFetchApi";
 
 export interface BlogPost {
-  title: string,
-  description: string,
-  created_at: string,
-  image: any, 
+  title: string;
+  description: string;
+  created_at: string;
+  image: any;
+  content: string;
 }
 
-export const fetchBlogPost = (id: number) => strapi
-  .get<BlogPost>(`/blog-posts/${id}`)
-  .then(({ data }) => data)
-  .then(({ title, description, created_at, image }) => ({ title, description, created_at, image }))
+export const fetchBlogPost = (id: number) =>
+  strapi
+    .get<BlogPost>(`/blog-posts/${id}`)
+    .then(({ data }) => data)
+    .then(({ title, description, created_at, image, content }) => ({
+      title,
+      description,
+      created_at,
+      image,
+      content,
+    }));
 
-export const useBlogPost = (id: number) => useFetchApi<BlogPost>(`/blog-posts/${id}`, () => fetchBlogPost(id))
+export const useBlogPost = (id: number) =>
+  useFetchApi<BlogPost>(`/blog-posts/${id}`, () => fetchBlogPost(id));
 
-export const fetchBlogPosts = () => strapi
-  .get<BlogPost[]>(`/blog-posts`)
-  .then(({ data }) => data)
-  .then(people => people
-    .map(({ title, description, created_at, image }) => ({ title, description, created_at, image }))
-  )
+export const fetchBlogPosts = () =>
+  strapi
+    .get<BlogPost[]>(`/blog-posts`)
+    .then(({ data }) => data)
+    .then((people) =>
+      people.map(({ title, description, created_at, image, content }) => ({
+        title,
+        description,
+        created_at,
+        image,
+        content,
+      }))
+    );
 
-export const useBlogPosts = () => useFetchApi<BlogPost[]>(`/blog-posts`, fetchBlogPosts)
+export const useBlogPosts = () =>
+  useFetchApi<BlogPost[]>(`/blog-posts`, fetchBlogPosts);
