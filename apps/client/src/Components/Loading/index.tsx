65c67548c1f47c096ab6bc053a4bccedfb8df2e1
priscila-eng <priscilaangell123@gmail.com>
2021-07-13 10:00:00 -0300

    ✨ 💄 Implements squareLoader and add colors

--- apps/client/src/Components/Loading/index.tsx
+++ apps/client/src/Components/Loading/index.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react'
+import SquareLoader from 'react-spinners/SquareLoader'
+
+import { Container, Color } from './style'
+
 
 export const Loading: FC = () => {
   return (
-    <div>loading...</div>
+    <Container>
+      <SquareLoader color ="#FC0061" css={Color}/>
+    </Container>
   )
 }
 
