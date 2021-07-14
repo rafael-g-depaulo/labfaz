ae29805616a6ed67f755a05a8254f751ef198b4b
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-13 22:29:51 -0300

    ✨ Feature: add a margin between Input and Label

---
+++ apps/client/src/Components/Input/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.6rem;
+
+  label {
+    font-weight: 600;
+    color: var(--color-text-black);
+    /* margin-bottom: -0.5rem; */
+    margin-bottom: 0.75rem;
+  }
+
+  input {
+    width: 12.4rem;
+    height: 2.4rem;
+    border: 0;
+    padding: 0.7rem 4.2rem 0.7rem 0.7rem;
+
+    &::placeholder {
+      color: rgba(0, 0, 0, 0.25);
+    }
+  }
+`
