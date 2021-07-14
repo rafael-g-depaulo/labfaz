b3a88bb8c5fe28c3d956ff13a6331295f747a178
priscila-eng <priscilaangell123@gmail.com>
2021-07-14 09:31:25 -0300

    💄 improve layout web

--- apps/client/src/Pages/NotFound/Web/style.ts
+++ apps/client/src/Pages/NotFound/Web/style.ts
@@ -1,22 +1,39 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { Title } from 'Components/Typography/Title'
+import { Rectangle } from 'Pages/Contruction/Web/style'
+import { DesktopSmall } from 'Utils/breakpoints'
 
 export const Container = styled.div`
   display: flex;
-  height: 100vh;
+  min-height: 100vh;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-between;
   background-color: var(--background-black);
 `;
 
 export const Content = styled.div`
-  padding: 0 12rem;
+  padding: 0 10rem;
+  ${DesktopSmall(css`
+    padding: 0 5rem;
+  `)}
+`;
+
+export const Rectangle2 = styled(Rectangle)`
+  max-width: 60rem;
+  padding: 0 3rem 0 3rem;
+  ${DesktopSmall(css`
+    padding: 0 1rem 0 3rem;
+  `)}
 `;
 
 export const Text = styled(Title)`
-  max-width: 16rem;
+  max-width: 14rem;
   color: var(--color-text-beige);
   font-size: var(--font-size-title);
   line-height: var(--line-height-default);
+
+  ${DesktopSmall(css`
+    max-width: 10rem;
+  `)}
 `;
