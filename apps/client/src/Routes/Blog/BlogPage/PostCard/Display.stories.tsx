2619a0a3870e9e9081a4ce876e5b8e8b7b16819b
clarice42 <claramaia820@gmail.com>
2021-07-19 10:33:17 -0300

    ✨ Add post details storybook

--- apps/client/src/Routes/Blog/BlogPage/PostCard/Display.stories.tsx
+++ apps/client/src/Routes/Blog/BlogPage/PostCard/Display.stories.tsx
@@ -8,6 +8,7 @@ import PostCardDisplay from './Display'
 // import Mobile from './Mobile'
 
 const samplePost: BlogPost = {
+  id: 1,
   title: "Teste de blog post",
   description: "Isso é um teste",
   created_at: "2021-06-29T23:50:54.596Z",
@@ -16,6 +17,7 @@ const samplePost: BlogPost = {
     "https://images.pexels.com/photos/8285483/pexels-photo-8285483.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
     name: "imagem aleatória",
   },
+  content: "Esse é o conteúdo do post!",
 }
 const posts = Array.from({ length: 4 }, () => samplePost)
 
