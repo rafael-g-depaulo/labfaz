e7324cc03445a2a1cacb760a759214f7b610e4c8
priscila-eng <priscilaangell123@gmail.com>
2021-07-21 10:33:14 -0300

    ✨ Add youtube

--- apps/client/src/Api/SocialNetworksLabfaz.ts
+++ apps/client/src/Api/SocialNetworksLabfaz.ts
@@ -6,12 +6,36 @@ export interface SocialNetworksLabfaz {
   twitter: string,
   googlePlus: string,
   linkedin: string,
-  instagram: string
+  instagram: string,
+  youtube: string,
+  ddd: number,
+  phone: number,
+  email: string
 }
 
 export const fetchSocialNetworksLabfaz: () => Promise<SocialNetworksLabfaz> = () => strapi
   .get(`/social-networks-labfaz`)
   .then(({ data }) => data)
-  .then(({ facebook, twitter, googlePlus, linkedin, instagram }) => ({ facebook, twitter, googlePlus, linkedin, instagram }))
+  .then(({
+    facebook,
+    twitter,
+    googlePlus,
+    linkedin,
+    instagram,
+    youtube,
+    ddd,
+    phone,
+    email
+  }) => ({
+    facebook,
+    twitter,
+    googlePlus,
+    linkedin,
+    instagram,
+    youtube,
+    ddd,
+    phone,
+    email
+  }))
 
 export const useSocialNetworksLabfaz = () => useFetchApi<SocialNetworksLabfaz>(`/social-networks-labfaz`, fetchSocialNetworksLabfaz)
