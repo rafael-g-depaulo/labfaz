6d0d455b075cb7ce421e4a50f7084227cc1529a3
priscila-eng <priscilaangell123@gmail.com>
2021-07-21 11:50:19 -0300

    ⚡️ Change unity

--- apps/client/src/Components/Loading/style.ts
+++ apps/client/src/Components/Loading/style.ts
@@ -5,8 +5,8 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: transparent;
 `
 
