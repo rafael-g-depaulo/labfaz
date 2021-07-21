159d2d07dc5dbfd412c5da1186fb49ff610ec4b4
priscila-eng <priscilaangell123@gmail.com>
2021-07-21 10:37:30 -0300

    ♻️  Improve footer component

--- apps/client/src/Components/Footer/Footer.stories.tsx
+++ apps/client/src/Components/Footer/Footer.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Footer from 'Components/Footer'
 
 storiesOf("Components/Footer", module)
   .addParameters({ component: Footer })
-  .add("footer", () => <Footer />)
+  .add("footer", () => <BrowserRouter><Footer /></BrowserRouter>)
--- apps/client/src/Components/Footer/index.tsx
+++ apps/client/src/Components/Footer/index.tsx
@@ -1,22 +1,32 @@
 import React, { FC } from 'react'
 
-import Logo from './Logo'
-import Navigation from './Navigation'
-import NewsLetter from './Newsletter'
-import Contact from './Contact'
-
-
-import { FooterContainer } from './style'
+import Web from './Web'
+import Mobile from './Mobile'
+// import { useSocialNetworksLabfaz } from 'Api/SocialNetworksLabfaz'
+// import Loading from 'Components/Loading'
+import useMobile from 'Hooks/useMobile'
 
 const Footer: FC = () => {
-  return (
-    <FooterContainer>
-      <Logo />
-      <Navigation />
-      <NewsLetter />
-      <Contact phoneNumber="(61) 91234-5678" email="contato@labfaz.com.br" />
-    </FooterContainer>
-  )
+  // const result = useSocialNetworksLabfaz()
+  const width = useMobile()
+
+  // if (result.isLoading) return <Loading />
+  // if (result.error) return <div>error: {result.error.message}</div>
+
+  const data = {
+    facebook: "https://www.facebook.com",
+    twitter: "https://www.twitter.com",
+    googlePlus: "https://www.google.com",
+    linkedin: "https://linkedin.com",
+    instagram: "https://instagram.com",
+    youtube: "https://youtube.com",
+    ddd: 61,
+    phone: 912345678,
+    email: "contato@labfaz.com.br"
+  }
+
+  if(width) return <Mobile data={data}/>
+  else return <Web data={data} />
 }
 
 export default Footer;
