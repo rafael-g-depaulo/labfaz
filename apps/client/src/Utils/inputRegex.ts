b0e98424fcae94a0351a9806d55f565e4464042e
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-07-28 00:47:39 -0300

    ✨ Feature: create regex logic and static datas

---
+++ apps/client/src/Utils/inputRegex.ts
@@ -0,0 +1,1 @@
+export const OnlyNumbers = (str: string) => str.replace(/[^0-9]/g, '')
---
+++ apps/client/src/Utils/selectOptionsData.ts
@@ -0,0 +1,428 @@
+export const CidadesDF = [
+  {
+    value: 'Brasília',
+    label: 'Brasíla',
+  },
+  {
+    value: 'Brazlândia',
+    label: 'Brazlândia',
+  },
+  {
+    value: 'Candangolândia',
+    label: 'Candangolândia',
+  },
+  {
+    value: 'Ceilândia',
+    label: 'Ceilândia',
+  },
+  {
+    value: 'Gama',
+    label: 'Gama',
+  },
+  {
+    value: 'Guará',
+    label: 'Guará',
+  },
+  {
+    value: 'Núcleo Bandeirante',
+    label: 'Núcleo Bandeirante',
+  },
+  {
+    value: 'Paranoá',
+    label: 'Paranoá',
+  },
+  {
+    value: 'Recanto das Emas',
+    label: 'Recanto das Emas',
+  },
+  {
+    value: 'Riacho Fundo I',
+    label: 'Riacho Fundo I',
+  },
+  {
+    value: 'Riacho Fundo II',
+    label: 'Riacho Fundo II',
+  },
+  {
+    value: 'Samambaia',
+    label: 'Samambaia',
+  },
+  {
+    value: 'Santa Maria',
+    label: 'Santa Maria',
+  },
+  {
+    value: 'São Sebastião',
+    label: 'São Sebastião',
+  },
+  {
+    value: 'Sobradinho',
+    label: 'Sobradinho',
+  },
+  {
+    value: 'Taguatinga',
+    label: 'Taguatinga',
+  }
+]
+
+export const CidadesEntorno = [
+  {
+    value: 'Abadiânia',
+    label: 'Abadiânia',
+  },
+  {
+    value: 'Água Fria de Goiás',
+    label: 'Água Fria de Goiás',
+  },
+  {
+    value: 'Alexânia',
+    label: 'Alexânia',
+  },
+  {
+    value: 'Cabeceiras',
+    label: 'Cabeceiras',
+  },
+  {
+    value: 'Cidade Ocidental',
+    label: 'Cidade Ocidental',
+  },
+  {
+    value: 'Cocalzinho de Goiás',
+    label: 'Cocalzinho de Goiás',
+  },
+  {
+    value: 'Corumbá de Goiás',
+    label: 'Corumbá de Goiás',
+  },
+  {
+    value: 'Cristalina',
+    label: 'Cristalina',
+  },
+  {
+    value: 'Formosa',
+    label: 'Formosa',
+  },
+  {
+    value: 'Luziânia',
+    label: 'Luziânia',
+  },
+  {
+    value: 'Mimoso de Goiás',
+    label: 'Mimoso de Goiás',
+  },
+  {
+    value: 'Novo Gama',
+    label: 'Novo Gama',
+  },
+  {
+    value: 'Padre Bernardo',
+    label: 'Padre Bernardo',
+  },
+  {
+    value: 'Pirenópolis',
+    label: 'Pirenópolis',
+  },
+  {
+    value: 'Planaltina',
+    label: 'Planaltina',
+  },
+  {
+    value: 'Santo Antônio do Descoberto',
+    label: 'Santo Antônio do Descoberto',
+  },
+  {
+    value: 'Valparaíso de Goiás',
+    label: 'Valparaíso de Goiás',
+  },
+  {
+    value: 'Vila Boa',
+    label: 'Vila Boa',
+  },
+  {
+    value: 'Unaí',
+    label: 'Unaí',
+  },
+  {
+    value: 'Buritis',
+    label: 'Buritis',
+  },
+  {
+    value: 'Arinos',
+    label: 'Arinos',
+  },
+  {
+    value: 'Cabeceira Grande',
+    label: 'Cabeceira Grande',
+  }
+]
+
+export const Estados = [
+  {
+    value: 'Acre',
+    label: 'Acre',
+  },
+  {
+    value: 'Alagoas',
+    label: 'Alagoas',
+  },
+  {
+    value: 'Amapá',
+    label: 'Amapá',
+  },
+  {
+    value: 'Amazonas',
+    label: 'Amazonas',
+  },
+  {
+    value: 'Bahia',
+    label: 'Bahia',
+  },
+  {
+    value: 'Ceará',
+    label: 'Ceará',
+  },
+  {
+    value: 'Espírito Santo',
+    label: 'Espírito Santo',
+  },
+  {
+    value: 'Goiás',
+    label: 'Goiás',
+  },
+  {
+    value: 'Maranhão',
+    label: 'Maranhão',
+  },
+  {
+    value: 'Mato Grosso',
+    label: 'Mato Grosso',
+  },
+  {
+    value: 'Mato Grosso do Sul',
+    label: 'Mato Grosso do Sul',
+  },
+  {
+    value: 'Minas Gerais',
+    label: 'Minas Gerais',
+  },
+  {
+    value: "Pará",
+    label: "Pará",
+  },
+  {
+    value: "Paraíba",
+    label: "Paraíba",
+  },
+  {
+    value: "Paraná",
+    label: "Paraná",
+  },
+  {
+    value: "Pernambuco",
+    label: "Pernambuco",
+  },
+  {
+    value: "Piauí",
+    label: "Piauí",
+  },
+  {
+    value: "Rio de Janeiro",
+    label: "Rio de Janeiro",
+  },
+  {
+    value: "Rio Grande do Norte",
+    label: "Rio Grande do Norte",
+  },
+  {
+    value: "Rio Grande do Sul",
+    label: "Rio Grande do Sul",
+  },
+  {
+    value: "Rondônia",
+    label: "Rondônia",
+  },
+  {
+    value: "Roraima",
+    label: "Roraima",
+  },
+  {
+    value: "Santa Catarina",
+    label: "Santa Catarina",
+  },
+  {
+    value: "São Paulo",
+    label: "São Paulo",
+  },
+  {
+    value: "Sergipe",
+    label: "Sergipe",
+  },
+  {
+    value: "Tocantins",
+    label: "Tocantins",
+  }
+]
+
+export const idiomOptions = [
+  {
+    label: 'Inglês',
+    value: 'Inglês',
+  },
+  {
+    label: 'Espanhol',
+    value: 'Espanhol',
+  },
+  {
+    label: 'Francês',
+    value: 'Francês',
+  },
+  {
+    label: 'Italiano',
+    value: 'Italiano',
+  },
+  {
+    label: 'Chinês Cantonês',
+    value: 'Chinês Cantonês',
+  },
+  {
+    label: 'Chines Mandarim',
+    value: 'Chines Mandarim',
+  },
+  {
+    label: 'Japonês',
+    value: 'Japonês',
+  },
+  {
+    label: 'Alemão',
+    value: 'Alemão',
+  },
+  {
+    label: 'LIBRAS (Língua Brasileira de Sinais)',
+    value: 'LIBRAS (Língua Brasileira de Sinais)',
+  },
+  {
+    label: 'LIBRAS tátil (Linguagem utilizada pelos surdo-cegos)',
+    value: 'LIBRAS tátil (Linguagem utilizada pelos surdo-cegos)',
+  },
+  {
+    label: 'Língua Inglesa de Sinais',
+    value: 'Língua Inglesa de Sinais',
+  },
+  {
+    label: 'Gestuno (Língua Internacional de Sinais)',
+    value: 'Gestuno (Língua Internacional de Sinais)',
+  },
+  {
+    label: 'Outro',
+    value: 'Outro',
+  },
+]
+
+export const formationOptions = [
+  {
+    label: 'Não tenho nenhuma formação escolar',
+    value: 'nao_tem',
+  },
+  {
+    label: 'Ensino fudamental 1 (até o 5º ano)',
+    value: 'ensino_fundamental_1',
+  },
+  {
+    label: 'Ensino fudamental 2 (até o 9º ano)',
+    value: 'ensino_fundamental_2',
+  },
+  {
+    label: 'Ensino Médio',
+    value: 'ensino_medio',
+  },
+  {
+    label: 'Ensino Médio em curso técnico profissionalizante',
+    value: 'ensino_medio_tecnico',
+  },
+  {
+    label: 'Curso Superior',
+    value: 'curso_superior',
+  },
+  {
+    label: 'Pós Graduação',
+    value: 'pos',
+  },
+]
+
+export const certificationOptionsPage1 = [
+  {
+    label: 'NR 01 – Disposições Gerais e Gerenciamento de Riscos Ocupacionais',
+    value: 'nr_01',
+  },
+  {
+    label:
+      'NR 04 – Serviços Especializados em Engenharia de Segurança e em Medicina do Trabalho',
+    value: 'nr_04',
+  },
+  {
+    label: 'NR 05 – Comissão Interna de Prevenção de Acidentes',
+    value: 'nr_05',
+  },
+  {
+    label: 'NR 06 – Equipamentos de Proteção Individual – EPI',
+    value: 'nr_06',
+  },
+  {
+    label:
+      'NR 07 – Programas de Controle Médico de Saúde Ocupacional – PCMSONR 07 – Nova – Programas de Controle Médico de Saúde Ocupacional – PCMSO (Vigência 09/03/2021)',
+    value: 'nr_07',
+  },
+  {
+    label:
+      'NR 09 – Programas de Prevenção de Riscos Ambientais NR 09 – Avaliação e Controle das Exposições Ocupacionais a Agentes Físicos, Químicos e Biológicos',
+    value: 'nr_09',
+  },
+  {
+    label: 'NR 10 – Segurança em Instalações e Serviços em Eletricidade',
+    value: 'nr_10',
+  },
+]
+
+export const certificationOptionsPage2 = [
+  {
+    label:
+      'NR 11 – Transporte, Movimentação, Armazenagem e Manuseio de Materiais',
+    value: 'nr_11',
+  },
+  {
+    label: 'NR 12 – Segurança no Trabalho em Máquinas e Equipamentos',
+    value: 'nr_12',
+  },
+  {
+    label: 'NR 15 – Atividades e Operações Insalubres',
+    value: 'nr_15',
+  },
+  {
+    label: 'NR 16 – Atividades e Operações Perigosas',
+    value: 'nr_16',
+  },
+  {
+    label: 'NR 20 – SST com Inflamáveis e Combustíveis',
+    value: 'nr_20',
+  },
+  {
+    label: 'NR 21 – Trabalho a Céu Aberto',
+    value: 'nr_21',
+  },
+  {
+    label: 'NR 23 – Proteção Contra Incêndios',
+    value: 'nr_23',
+  },
+  {
+    label: 'NR 24 – Condições Sanitárias e de Conforto nos Locais de Trabalho',
+    value: 'nr_24',
+  },
+  {
+    label: 'NR 26 – Sinalização de Segurança',
+    value: 'nr_26',
+  },
+  {
+    label: 'NR 35 – Trabalho em Altura',
+    value: 'nr_35',
+  },
+]
