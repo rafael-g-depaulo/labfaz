38ff73bd2b6e918be5bce3d5fab478cc02222b9b
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-07-29 12:11:59 -0300

    💄 Update Construction stories

--- apps/client/src/Pages/Contruction/Construction.stories.tsx
+++ apps/client/src/Pages/Contruction/Construction.stories.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { BrowserRouter } from 'react-router-dom'
+
+import { SocialNetworksLabfaz } from 'Api/SocialNetworksLabfaz'
 
 import Construction from './'
+import Web from './Web'
+import Mobile from './Mobile'
+
+const data: SocialNetworksLabfaz = {
+  email: "test@test.com",
+  phone: "619999999",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  googlePlus: "https://googlePlus.com",
+}
 
 storiesOf("Pages/Construction", module)
   .addParameters({ component: Construction })
-  .add("construction", () => <BrowserRouter><Construction /></BrowserRouter>)
+  .add("responsive", () => <Construction />)
+  .add("web", () => <Web data={data} />)
+  .add("mobile", () => <Mobile data={data} />)
