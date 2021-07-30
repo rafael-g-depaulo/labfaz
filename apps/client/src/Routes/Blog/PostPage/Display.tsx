41db2da71bbff76facf560c4f32e2f0d3a663e48
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-07-30 11:30:06 -0300

    ♻️ Refactor show blogpost page

--- apps/client/src/Routes/Blog/PostPage/Display.tsx
+++ apps/client/src/Routes/Blog/PostPage/Display.tsx
@@ -2,23 +2,28 @@ import React, {FC} from 'react'
 
 import Header from 'Components/Header'
 import Banner from 'Components/Banner'
-import PostDetails from './PostDetails';
+import Footer from 'Components/Footer'
+
 import { BlogBannerInfo } from 'Api/BlogBannerInfo'
+import { BlogPost } from 'Api/BlogPost'
+
+import PostDetails from './PostDetails/Display'
 
 export interface DisplayProps {
   data: BlogBannerInfo;
-  id: number;
+  post: BlogPost,
 }
 
 export const Display: FC<DisplayProps> = ({
-  data, id,
+  data, post,
 }) => {
-  const {title, subtitle} = data!
+  const { title, subtitle } = data
   return (
     <>
       <Header />
       <Banner title={title} subtitle={subtitle} align="left" />
-      <PostDetails id={id}/>
+      <PostDetails post={post}/>
+      <Footer />
     </>
   )
 }
--- apps/client/src/Routes/Blog/PostPage/index.tsx
+++ apps/client/src/Routes/Blog/PostPage/index.tsx
@@ -3,19 +3,22 @@ import React, { FC } from "react";
 import Loading from "Components/Loading";
 import Display from "./Display"
 import { useBlogBannerInfo } from "Api/BlogBannerInfo";
+import { useBlogPost } from "Api/BlogPost";
 
 interface RouteParams {
   id: number;
 }
 
-export const PostPage: FC<RouteParams> = ({id}) => {  
-  const result = useBlogBannerInfo();
+export const PostPage: FC<RouteParams> = ({ id }) => {  
+  const result = useBlogBannerInfo()
+  const post = useBlogPost(id)
 
-  if (result.isLoading) return <Loading />;
-  if (result.error) return <div>error: {result.error.message}</div>;
+  if (result.isLoading || post.isLoading) return <Loading />
+  if (result.error) return <div>error: {result.error.message}</div>
+  if (post.error) return <div>error: {post.error.message}</div>
 
   return (
-    <Display data={result.data!} id={id}/>
+    <Display data={result.data} post={post.data} />
   );
 };
 
