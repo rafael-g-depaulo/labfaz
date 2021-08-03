458cf4c01334f602ccf6bb326ef84895f355784a
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-03 12:09:37 -0300

    🔧 Update jest config

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -16,38 +16,4 @@ module.exports = {
     "<rootDir>/node_modules/"
   ],
 
-  // for test reports
-  // TODO: chose between one of them
-  reporters: [
-    "default",
-    [
-      "jest-html-reporter",
-      {
-        pageTitle: "Test Report",
-        // append: true,
-        outputPath: "./coverage/jest-html-reporter/test-report.html",
-      },
-    ],
-    [
-      "jest-stare",
-      {
-        resultDir: "./coverage/jest-stare",
-        reportTitle: "jest-stare!",
-        additionalResultsProcessors: [
-          "jest-junit",
-        ],
-        coverageLink: "../lcov-report/index.html",
-        jestStareConfigJson: "jest-stare.json",
-        jestGlobalConfigJson: "globalStuff.json",
-      },
-    ],
-    [
-      "jest-html-reporters",
-      {
-        publicPath: "./coverage/jest-html-reporters",
-        filename: "report.html",
-        expand: true,
-      },
-    ],
-  ],
 }
--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,22 @@ module.exports = {
     "/node_modules/",
     "/dist/",
   ],
+
+  collectCoverageFrom: [
+    "**/src/**/*.{ts,tsx}",
+    "!<rootDir>/node_modules/",
+    "!**/src/**/*.stories.{ts,tsx}"
+  ],
+
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./coverage/jest-html-reporters",
+        filename: "report.html",
+        expand: true,
+      },
+    ],
+  ],
 }
