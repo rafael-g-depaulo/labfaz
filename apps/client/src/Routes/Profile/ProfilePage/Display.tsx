ad99c11418613fce59193a0abfcbae935fd00cad
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-08-03 21:00:03 -0300

    ✨ Feature: create Display and index to Profile Page

---
+++ apps/client/src/Routes/Profile/ProfilePage/Display.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+
+import Header from 'Components/Header'
+import Profile from 'Components/Profile'
+
+export const Display: FC = () => {
+
+  return (
+    <>
+      <Header />
+      <Profile />
+    </>
+  )
+}
+
+export default Display
---
+++ apps/client/src/Routes/Profile/ProfilePage/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+import Display from './Display'
+
+export const ProfilePage: FC = () => {
+
+  return <Display />
+}
+
+export default ProfilePage
