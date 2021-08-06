954036b3bd4e9355de355e831844ff1911953d77
zipudhe <lucasramalho29@gmail.com>
2021-08-06 18:55:47 -0300

    Revert "🚧 setting up adminBro with auth router"
    This reverts commit 351493606c6406124fdc8bbcadf6d04069908093.

--- apps/api/src/Admin/adminBro.ts
+++ apps/api/src/Admin/adminBro.ts
@@ -7,6 +7,7 @@ import AnimalExample from 'Entities/AnimalExample';
 import { getResources, makeConnections } from './resources'
 
 // Vai precisar adicionar class validador ao adminBro caso use
+
 AdminBro.registerAdapter( { Database, Resource } )
 
 
@@ -14,7 +15,7 @@ export const getAdminBro = (conn: Connection) => {
     makeConnections(conn)
     AnimalExample.useConnection(conn)
     return new AdminBro({
-      databases: [conn],
+      // databases: [conn],
       resources: getResources(),
       branding: {
         companyName: "Labfaz",
@@ -25,17 +26,7 @@ export const getAdminBro = (conn: Connection) => {
 }
 
 const getAdminRouter = (adminBro: AdminBro) => {
-  return AdminBroExpress.buildAuthenticatedRouter(adminBro, {
-    authenticate: (email, password) => {
-      console.log(`email: ${email}, password: ${password}`)
-      return {
-        email: email
-      }
-      
-    },
-    cookieName: "cookiename",
-    cookiePassword: 'strongpassword'
-  });
+  return AdminBroExpress.buildRouter(adminBro);
 }
 
 export default getAdminRouter;
