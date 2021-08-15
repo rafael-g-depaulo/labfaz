0bf8d61750cae6b5cee010a9c8083225cc9b915d
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-15 14:26:24 -0300

    ✅ Update test and add storybook

--- apps/client/src/Routes/Home/HomePage/Display.stories.tsx
+++ apps/client/src/Routes/Home/HomePage/Display.stories.tsx
@@ -1,10 +1,20 @@
-// import React from 'react'
-// import { storiesOf } from '@storybook/react'
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-// import HomePage from './Display'
+import HomePage from "./Display";
+import { HomepageBannerInfo } from "Api/HomepageBannerInfo";
+import { mockImage } from "Utils/Image";
 
-// storiesOf("Pages/Home", module)
-//   .addParameters({ component: HomePage })
-//   .add("default", () => <HomePage data={{ msg: "Mocked Test" }}/>)
+const data: HomepageBannerInfo = {
+  title: "LABFAZ",
+  subtitle: "LoremIpsum",
+  image: mockImage({
+    url:
+      "https://labfaz-strapi-assets.s3.sa-east-1.amazonaws.com/Whats_App_Image_2020_12_19_at_17_23_28_439c4529a0.jpeg",
+    alternativeText: "Blog Banner Image",
+  }),
+};
 
-export {}
+storiesOf("Pages/Home", module)
+  .addParameters({ component: HomePage })
+  .add("list posts", () => <HomePage data={data} />);
--- apps/client/src/Routes/Home/HomePage/Display.test.tsx
+++ apps/client/src/Routes/Home/HomePage/Display.test.tsx
@@ -1,24 +1,47 @@
-import React from 'react'
-import render from "Utils/render"
+import React from "react";
+import render from "Utils/render";
+import { BrowserRouter } from "react-router-dom";
 
-import HomePageDisplay from "./Display"
-import { HelloExample } from 'Api/HelloApiExample'
+import HomePageDisplay from "./Display";
+import { HomepageBannerInfo } from "Api/HomepageBannerInfo";
+
+const mockedData: HomepageBannerInfo = {
+  title: "LABFAZ",
+  subtitle:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta ligula nibh, nec interdum nunc maximus at.",
+  image: {
+    url:
+      "https://labfaz-strapi-assets.s3.sa-east-1.amazonaws.com/Whats_App_Image_2020_12_19_at_17_23_28_439c4529a0.jpeg",
+    alternativeText: "Homepage Banner Image",
+    caption: "string",
+    width: 20,
+    height: 20,
+    ext: "jpeg",
+  },
+};
 
 describe("Home Page", () => {
   it("renders without exploding", () => {
-    const mockedData: HelloExample = {
-      msg: ""
-    }
-    expect(() => render(<HomePageDisplay data={mockedData} />)).not.toThrow()
-  })
+    expect(() =>
+      render(
+        <BrowserRouter>
+          <HomePageDisplay data={mockedData} />{" "}
+        </BrowserRouter>
+      )
+    ).not.toThrow();
+  });
 
   it("displays the data message", () => {
-    const mockedData: HelloExample = {
-      msg: "mocked data test"
-    }
-    const { getByText } = render(<HomePageDisplay data={mockedData} />)
+    const { getByRole } = render(
+      <BrowserRouter>
+        <HomePageDisplay data={mockedData} />
+      </BrowserRouter>
+    );
 
     // expect retrieved data to be displayed
-    getByText(/"msg": "mocked data test"/)
-  })
-})
+    expect(getByRole("heading", { level: 1 })).toHaveTextContent("LABFAZ");
+    expect(getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta ligula nibh, nec interdum nunc maximus at."
+    );
+  });
+});
