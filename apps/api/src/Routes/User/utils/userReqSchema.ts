54e494c8fd15ac2062fa5b049bef289db53573e3
Joao Victor Cabral de Melo <joao.victor.cabralmelo@gmail.com>
2021-08-18 20:07:53 -0300

    ✨ Add: Schema to all objects

--- apps/api/src/Routes/User/utils/userReqSchema.ts
+++ apps/api/src/Routes/User/utils/userReqSchema.ts
@@ -1,5 +1,8 @@
-import  * as yup from "yup"
-import { Race, ShowName } from "Entities/Artist"
+import * as yup from "yup";
+import { Race, ShowName } from "Entities/Artist";
+import { Residency } from "Entities/Address";
+import { Formation } from "Entities/Technical";
+import { TechFormation } from "Entities/Area";
 
 export const userSchema = yup.object({
   email: yup.string().required().email(),
@@ -10,16 +13,88 @@ export const userSchema = yup.object({
     social_name: yup.string(),
     artistic_name: yup.string().min(4).max(10),
     gender: yup.string().required(),
-    cpf: yup.string().required().matches(/\d{3}\.\d{3}\.\d{3}-\d{2}/),
+    cpf: yup
+      .string()
+      .required()
+      .matches(/\d{3}\.\d{3}\.\d{3}-\d{2}/),
     birthday: yup.date().required(),
     rg: yup.string().required(),
     expedition_department: yup.string().required(),
     is_trans: yup.boolean().required(),
     race: yup.mixed<Race>().required().oneOf(Object.values(Race)),
     show_name: yup.mixed<ShowName>().required().oneOf(Object.values(ShowName)),
+    address: yup.object({
+      city: yup.string().required(),
+      cep: yup.string().required(),
+      neighbourhood: yup.string().required(),
+      number: yup.number().integer().positive().required(),
+      complement: yup.string().required(),
+      residency: yup
+        .mixed<Residency>()
+        .required()
+        .oneOf(Object.values(Residency)),
+    }),
+    contact: yup.object({
+      whatsapp: yup.string().required(),
+      twitter: yup.string().required(),
+      facebook: yup.string().required(),
+      instagram: yup.string().required(),
+      linkedin: yup.string().required(),
+      tiktok: yup.string().required(),
+      youtube: yup.string().required(),
+    }),
+    technical: yup.object({
+      formation: yup
+        .mixed<Formation>()
+        .required()
+        .oneOf(Object.values(Formation)),
+      is_drt: yup.boolean().required().default(false),
+      is_ceac: yup.boolean().required().default(false),
+      is_cnpj: yup.boolean().required().default(false),
+      is_affiliated: yup.boolean().required().default(false),
+      want_be_affiliated: yup.boolean().required().default(true),
+      areas: yup
+        .array()
+        .ensure()
+        .of(
+          yup.object({
+            technical_formation: yup
+              .mixed<TechFormation>()
+              .required()
+              .oneOf(Object.values(TechFormation)),
+            name: yup.string().required(),
+            describe: yup.string().required(),
+            started_year: yup.string().required(),
+            curriculum: yup
+              .array()
+              .ensure()
+              .of(
+                yup.object({
+                  url: yup.string().required().url(),
+                })
+              ),
+            certificate: yup
+              .array()
+              .ensure()
+              .of(
+                yup.object({
+                  name: yup.string().required(),
+                })
+              ),
+          })
+        ),
+      idiom: yup
+        .array()
+        .ensure()
+        .of(
+          yup.object({
+            name: yup.string().required(),
+          })
+        ),
+    }),
   }),
-})
+});
 
 export interface UserInfo extends yup.Asserts<typeof userSchema> {}
 
-export default userSchema
+export default userSchema;
