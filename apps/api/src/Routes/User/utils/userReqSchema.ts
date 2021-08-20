424c09a4a7182c7916f676cfe79175c42dbeb12c
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-20 11:35:27 -0300

    🔥 💡 comment out user Schema

--- apps/api/src/Routes/User/utils/userReqSchema.ts
+++ apps/api/src/Routes/User/utils/userReqSchema.ts
@@ -76,29 +76,31 @@ export const userContactSchema = yup.object().required().shape({
   youtube: yup.string().required(),
 })
 
+export const userArtistSchema = yup.object().required().shape({
+  photo_url: yup.string().required().url(),
+  name: yup.string().required(),
+  social_name: yup.string(),
+  artistic_name: yup.string().min(4).max(10),
+  gender: yup.string().required(),
+  cpf: yup
+    .string()
+    .required()
+    .matches(/\d{3}\.\d{3}\.\d{3}-\d{2}/),
+  birthday: yup.date().required(),
+  rg: yup.string().required(),
+  expedition_department: yup.string().required(),
+  is_trans: yup.boolean().required(),
+  race: yup.mixed<Race>().required().oneOf(Object.values(Race)),
+  show_name: yup.mixed<ShowName>().required().oneOf(Object.values(ShowName)),
+  address: addressSchema,
+  contact: userContactSchema,
+  technical: userTechnicalSchema,
+})
+
 export const userSchema = yup.object({
   email: yup.string().required().email(),
   password: yup.string().required().min(6),
-  artist: yup.object().required().shape({
-    photo_url: yup.string().required().url(),
-    name: yup.string().required(),
-    social_name: yup.string(),
-    artistic_name: yup.string().min(4).max(10),
-    gender: yup.string().required(),
-    cpf: yup
-      .string()
-      .required()
-      .matches(/\d{3}\.\d{3}\.\d{3}-\d{2}/),
-    birthday: yup.date().required(),
-    rg: yup.string().required(),
-    expedition_department: yup.string().required(),
-    is_trans: yup.boolean().required(),
-    race: yup.mixed<Race>().required().oneOf(Object.values(Race)),
-    show_name: yup.mixed<ShowName>().required().oneOf(Object.values(ShowName)),
-    address: addressSchema,
-    contact: userContactSchema,
-    technical: userTechnicalSchema,
-  }),
+  // artist: userArtistSchema,
 });
 
 export interface UserInfo extends yup.Asserts<typeof userSchema> {}
