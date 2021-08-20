948e726e4afcfa0231338590280357c1a21eb00e
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-20 11:43:20 -0300

    ✨ Add databaseError endpoint return

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -22,17 +22,14 @@ Db()
 
   const port = process.env.PORT ?? 5000
   
-  app.use("/", Routes({ conn }))
-
+  // Foi preciso colocar o admin bro antes dos middlewares por conta do body parser
   const adminBro = getAdminBro(conn)
-
   app.use(adminBro.options.rootPath, getAdminRouter(adminBro))
 
-  // Foi preciso colocar os middlewares depois do admin bro por conta do body parser
   Middewares(app)
 
+  app.use("/", Routes({ conn }))
   app.get('/hello', (_, res) => actionSuccessful(res, { msg: 'world' }))
-    
   app.use("*", (_, res) => badRequestError(res, "Route doesn't exist" ))
 
   app.listen(port, () => console.log(`listening PORT ${port}, in typescript!`)) 
