157edf8d5df90fef25b16b77937093870f762ed3
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-19 21:34:30 -0300

    🐛 Fix youtube player component reload

--- apps/client/src/Components/YoutubePlayer/index.tsx
+++ apps/client/src/Components/YoutubePlayer/index.tsx
@@ -1,33 +1,25 @@
-import React, { FC } from 'react'
+import React, { FC, useMemo } from 'react'
 import YouTube from 'react-youtube'
 
 export interface YoutubePlayerProps {
   href: string,
 }
 
+export const urlRegex = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?(?<id>[^#&?]*).*/
+
+const options = {
+  width: '100%',
+  height: '100%',
+  playerVars: {},
+}
+
 export const YoutubePlayer: FC<YoutubePlayerProps> = ({
   href,
 }) => {
-  let position, id;
-  
-  const options = {
-    width: '100%',
-    height: '100%',
-    playerVars: {},
-  }
-
-  if (href.substring(7,15) === "youtu.be") {
-    position = href.lastIndexOf('be/')
-    id = href.substr(position+3)
-  } else if (href.substring(16,21) === "embed") {
-    position = href.lastIndexOf('embed/')
-    id = href.substr(position+6)
-  } else {
-    position = href.lastIndexOf('watch?v=')
-    id = href.substr(position+8)
-  }
-
+  const id = useMemo(() => urlRegex.exec(href)?.groups?.id, [href])
   return (
     <YouTube videoId={id} opts={options}/>
   )
 }
+
+export default YoutubePlayer
