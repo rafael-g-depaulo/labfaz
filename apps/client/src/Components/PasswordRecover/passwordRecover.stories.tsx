f641451b21f9516afbed3e50ef3b481b0e7a1a96
zipudhe <lucasramalho29@gmail.com>
2021-08-23 15:19:44 -0300

    ✨ updates stories for passWord recover

--- apps/client/src/Components/PasswordRecover/passwordRecover.stories.tsx
+++ apps/client/src/Components/PasswordRecover/passwordRecover.stories.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import RecoverForm from '.';
+import PasswordChange from "./passwordChange"
 
 import { storiesOf } from "@storybook/react";
 
 
 storiesOf("Components/Recover", module)
   .add("recover", () => <RecoverForm />)
+
+
+  storiesOf("Components/Recover", module)
+  .add("confirmed email", () => <PasswordChange />)
