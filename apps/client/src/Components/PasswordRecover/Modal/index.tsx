927a2093dcf7203af7b8e48a25e04fb01386ded7
zipudhe <lucasramalho29@gmail.com>
2021-08-24 16:03:20 -0300

    ✨ 📱 feature modal responsive

--- apps/client/src/Components/PasswordRecover/Modal/index.tsx
+++ apps/client/src/Components/PasswordRecover/Modal/index.tsx
@@ -5,6 +5,8 @@ import { Title } from "Components/Typography/Title"
 import { Text } from "Components/Typography/Text"
 import SocialMediaIcons  from "Components/SocialMediaIcons"
 
+import useMobile from 'Hooks/useMobile';
+
 interface ModalProps {
   isVisible: boolean,
   setFunction: React.Dispatch<React.SetStateAction<boolean>>
@@ -16,7 +18,7 @@ export const Modal: FC<ModalProps> = ({ isVisible, setFunction }) => {
   return(
     <Container isVisible={isVisible}>
       <ModalDiv>
-        <Title level={3}> Pedimos desculpas pelos problemas </Title>
+        <Title level={3}> {useMobile() ? "Pedimos desculpas" : "Pedimos desculpas pelos problemas"}   </Title>
         <Text> O email com as instrucoes para recuperar sua senha foram enviados. Cheque sua caixa de mensagens ou em alguns minutos tente novamente. </Text>
         <Text> (99)9999-999 </Text>
         <Text> contato@labfaz.com.br </Text>
