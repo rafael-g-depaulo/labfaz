d26738d53f6c7088206144eb08ff368301954085
zipudhe <lucasramalho29@gmail.com>
2021-08-24 21:55:49 -0300

    ✨ feature admin Entity

--- apps/api/src/Entities/Admin.ts
+++ apps/api/src/Entities/Admin.ts
@@ -3,15 +3,26 @@ import {
   Entity,
   PrimaryColumn,
   BaseEntity,
+<<<<<<< HEAD
   CreateDateColumn,
   BeforeInsert
 } from "typeorm"
 import { nanoid } from "nanoid";
+=======
+  CreateDateColumn
+} from "typeorm"
+
+export interface IAdmin {
+
+}
+
+>>>>>>> a9fccd8 (✨ feature admin Entity)
 @Entity()
 export class Admin extends BaseEntity {
   @PrimaryColumn()
   id: string;
 
+<<<<<<< HEAD
   @Column()
   email: string
 
@@ -28,6 +39,13 @@ export class Admin extends BaseEntity {
   addId() {
     this.id = nanoid();
   }
+=======
+  @Column({ default: "admin" })
+  admin: "admin" | "professor"
+
+  @CreateDateColumn()
+  created_at: Date
+>>>>>>> a9fccd8 (✨ feature admin Entity)
 }
 
 export default Admin
