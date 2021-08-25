9e8119752607e484396504842c3589199bda3da7
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-25 20:33:30 -0300

    🚧 Prepare work to use Pagination item

--- apps/client/src/Routes/Blog/BlogPage/PostCard/Display.tsx
+++ apps/client/src/Routes/Blog/BlogPage/PostCard/Display.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from "react"
 
-import { Container, Content } from "./styles"
-
 import { BlogPost } from "Api/BlogPost"
+// import Pagination from "Components/Pagination/Static"
 
+import { Container, Content } from "./styles"
 import PostItem from "./PostItem"
 
 interface DisplayProps {
@@ -14,7 +14,15 @@ export const Display: FC<DisplayProps> = ({ posts }) => {
   return (
     <Container>
       <Content>
-        {posts.map((post, index) => <PostItem post={post} key={index} />)}
+        {/* <Pagination items={posts}>
+        {renderedPosts => renderedPosts.map((post, index) => (
+          <PostItem post={post} key={index} />
+        ))}
+        </Pagination> */}
+        
+        {posts.map((post, index) => (
+          <PostItem post={post} key={index} />
+        ))}
       </Content>
     </Container>
   )
