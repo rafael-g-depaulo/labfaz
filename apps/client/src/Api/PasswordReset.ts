598bfbc4e58aa495a44557fb67a499ebe8edb8a9
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-26 11:32:13 -0300

    ✏️ Fix incorrect use of axios lib. Change ask-reset to post

--- apps/client/src/Api/PasswordReset.ts
+++ apps/client/src/Api/PasswordReset.ts
@@ -1,7 +1,5 @@
 import { api } from "Api"
 
-import { useFetchApi } from "Hooks/useFetchApi"
-
 interface AskResetPassResponseData {
   status: string,
   code: number,
@@ -16,24 +14,10 @@ const headers = {
   "Content-Type": "application/json"
 }
 
-export const askResetPassword: (email: string ) => Promise<AskResetPassResponseData> = (email) => api
-.get<AskResetPassResponseData>('/sessions/ask-reset', {
-  headers: headers,
-  data: {
-    email
-  },
-})
-.then(({data}) => data)
+export const askResetPassword = (email: string) => api
+  .post<AskResetPassResponseData>('/sessions/ask-reset', { email }, { headers })
+  .then(({data}) => data)
 
-export const useAskResetPassword = (email: string) => useFetchApi<AskResetPassResponseData>('/ask-reset', () => askResetPassword(email));
-
-
-export const resetPassword: (password: string, token: string) => Promise<ResetPassData> = (password: string, token: string) => api
-.post<ResetPassData>('/sessions/reset-password', {
-    password: password,
-    token: token
-})
-.then(({data}) => data)
-.catch((error) => error)
-
-export const useResetPassowrd = (password: string, token: string) => useFetchApi<ResetPassData>('/ask-reset', () => resetPassword(password, token));
+export const resetPassword = (password: string, token: string) => api
+  .post<ResetPassData>('/sessions/reset-password', { password, token })
+  .then(({data}) => data)
