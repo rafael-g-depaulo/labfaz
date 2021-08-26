d47489fe9e4176a6e84728a9a7fa5946a247f059
zipudhe <lucasramalho29@gmail.com>
2021-08-26 13:15:22 -0300

    ✨ feature admin creation

--- apps/api/src/Admin/resources/adminResource.ts
+++ apps/api/src/Admin/resources/adminResource.ts
@@ -1,23 +1,38 @@
 import Admin from '../../Entities/Admin'
-import { BaseRecord, ResourceWithOptions } from 'adminjs'
+import { BaseRecord, BaseResource, ResourceWithOptions } from 'adminjs'
+import { Connection } from 'typeorm'
+import AdminRepository from 'Repository/AdminRepository'
 
-const adminResource = (): ResourceWithOptions => ({
-  resource: Admin,
-  options: {
-    listProperties: ["name"],
-    actions: {
-      new: {
-        handler: async (request, response, context) => {
-          const { currentAdmin } = context
-          const record = context.record as BaseRecord
+interface RequestData {
+  email: string,
+  password: string,
+  role: "admin" | "professor"
+}
 
-          console.log("RECORD: " + record)
-          return response;
+
+const adminResource = (conn: Connection): ResourceWithOptions => {
+
+  const adminRepo = conn.getCustomRepository(AdminRepository)
+
+  return ({
+    resource: Admin,
+    options: {
+      listProperties: ["email"],
+      actions: {
+        new: {
+          isAccessible: ({ currentAdmin }) => {
+            if(currentAdmin) {
+              if(currentAdmin.title == "SuperAdmin") {
+                return true
+              }
+            }
+            return false
+          },
         }
       }
-    }
-  },
-
-})
+    },
+  
+  })
+}
 
 export default adminResource
