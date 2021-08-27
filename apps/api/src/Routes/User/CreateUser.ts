bff16565953f15fe880c55624c3cc53887436ce1
Joao Victor Cabral de Melo <joao.victor.cabralmelo@gmail.com>
2021-08-27 18:51:02 -0300

    ✨ Add: New Insomnia

--- apps/api/src/Routes/User/CreateUser.ts
+++ apps/api/src/Routes/User/CreateUser.ts
@@ -71,8 +71,8 @@ export const CreateUser: (
 
     const files = await UploadFiles([...curriculum, ...profilePicture ]);
 
-    const artistCurriculum = files.find((file) => file.fieldname === "curriculum");
-    const artistProfilePicture = files.find((file) => file.fieldname === "profilePicture")
+    const artistCurriculum = files.find((file) => file.fieldname === "curriculum")!;
+    const artistProfilePicture = files.find((file) => file.fieldname === "profilePicture")!;
       
   
 <<<<<<< HEAD
