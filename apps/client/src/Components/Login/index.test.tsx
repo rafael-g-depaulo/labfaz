5c6bc943d1b303d4eb2ca32391c64dd7653fa513
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-08-29 09:33:28 -0300

    ✨ Feat: create test for LoginPage

---
+++ apps/client/src/Components/Login/index.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import render from 'Utils/render'
+import { Login } from './index'
+
+it('renders login component', () => {
+
+  expect(() =>
+    render(
+      <BrowserRouter>
+          <Login onSubmit={() => {}} />
+      </BrowserRouter>
+    )
+  ).not.toThrow()
+})
+
+describe('Check content of Login component', () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <Login onSubmit={() => {}} />
+    </BrowserRouter>
+  )
+
+  it('checks if title rendered', () => {
+    const title = getByText('Seja bem vinde!')
+    expect(title).toHaveTextContent('Seja bem vinde!')
+  })
+
+  it('checks if subtitle rendered', () => {
+    const title = getByText(
+      'Laboratorio dos Fazeres e Saberes Tecnicos da Economia Criativa'
+    )
+    expect(title).toHaveTextContent(
+      'Laboratorio dos Fazeres e Saberes Tecnicos da Economia Criativa'
+    )
+  })
+})
+
+describe('Check user session links', () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <Login onSubmit={() => {}} />
+    </BrowserRouter>
+  )
+  it('check SignIn button', () => {
+    expect(getByText('ENTRAR').closest('button')).toHaveTextContent('ENTRAR')
+  })
+
+  it('check SignUp button', () => {
+    expect(getByText('CADASTRE-SE').closest('a')).toHaveAttribute(
+      'href',
+      '/SignUp'
+    )
+  })
+
+  it('check Recover link', () => {
+    expect(getByText('Esqueceu sua senha?').closest('a')).toHaveAttribute(
+      'href',
+      '/recover'
+    )
+  })
+})
