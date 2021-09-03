93f21a933a386d195f42a63cd045de7941b790cb
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-09-03 01:13:47 -0300

    ✨ Add test to display login Compoenent

---
+++ apps/client/src/Routes/Login/LoginPage/Display.test.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import render from 'Utils/render'
+import Display from './Display'
+
+it('renders login component', () => {
+  expect(() =>
+    render(
+      <BrowserRouter>
+        <Display onSubmit={() => {}} buttonType="button" />
+      </BrowserRouter>
+    )
+  ).not.toThrow()
+})
+
+describe('Check content of Login component', () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <Display onSubmit={() => {}} buttonType="button" />
+    </BrowserRouter>
+  )
+
+  it('checks if title rendered', () => {
+    const title = getByText('Seja bem vinde!')
+    expect(title).toHaveTextContent('Seja bem vinde!')
+  })
+
+  it('checks if subtitle rendered', () => {
+    const title = getByText(
+      'Laboratorio dos Fazeres e Saberes Tecnicos da Economia Criativa'
+    )
+    expect(title).toHaveTextContent(
+      'Laboratorio dos Fazeres e Saberes Tecnicos da Economia Criativa'
+    )
+  })
+})
+
+describe('Check user session links', () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <Display onSubmit={() => {}} buttonType="button" />
+    </BrowserRouter>
+  )
+
+  it('check Recover link', () => {
+    expect(getByText('Esqueceu sua senha?').closest('a')).toHaveAttribute(
+      'href',
+      '/recover'
+    )
+  })
+})
