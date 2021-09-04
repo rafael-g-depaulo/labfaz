6654501da44d0d8573c1ab10da6bc1b368118a7a
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-09-04 20:18:19 -0300

    🐛 🔥 Fix bad migrations

--- apps/api/src/Migrations/1630796625866-MakeCurriculumOptional.ts
+++ apps/api/src/Migrations/1630796625866-MakeCurriculumOptional.ts
@@ -4,9 +4,6 @@ export class MakeCurriculumOptional1630796625866 implements MigrationInterface {
     name = 'MakeCurriculumOptional1630796625866'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "artist" DROP COLUMN "gender_specifics"`);
-        await queryRunner.query(`DROP TYPE "public"."artist_gender_specifics_enum"`);
-        await queryRunner.query(`ALTER TABLE "artist" ADD "is_trans" boolean NOT NULL`);
         await queryRunner.query(`ALTER TABLE "artist" ALTER COLUMN "curriculum" DROP NOT NULL`);
         await queryRunner.query(`COMMENT ON COLUMN "artist"."curriculum" IS NULL`);
     }
@@ -14,9 +11,6 @@ export class MakeCurriculumOptional1630796625866 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`COMMENT ON COLUMN "artist"."curriculum" IS NULL`);
         await queryRunner.query(`ALTER TABLE "artist" ALTER COLUMN "curriculum" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "artist" DROP COLUMN "is_trans"`);
-        await queryRunner.query(`CREATE TYPE "public"."artist_gender_specifics_enum" AS ENUM('cisgênero', 'transgênero', 'não-binário', 'prefiro não responder')`);
-        await queryRunner.query(`ALTER TABLE "artist" ADD "gender_specifics" "artist_gender_specifics_enum" NOT NULL DEFAULT 'prefiro não responder'`);
     }
 
 }
