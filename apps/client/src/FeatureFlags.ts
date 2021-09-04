0f35afab106caae0dafa0647ea80e74a0f0e9e16
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-09-04 17:57:36 -0300

    🚩 Add feature flag for react-query devtools

--- apps/client/src/FeatureFlags.ts
+++ apps/client/src/FeatureFlags.ts
@@ -7,3 +7,5 @@ export const showRoutes = useEnvVar("REACT_APP_SHOW_ROUTES", true)
 export const showHome = useEnvVar("REACT_APP_SHOW_HOME", true)
 export const showBlog = useEnvVar("REACT_APP_SHOW_BLOG", true)
 export const showAboutUs = useEnvVar("REACT_APP_SHOW_ABOUT_US", true)
+
+export const showReactQueryDevtools = useEnvVar("REACT_APP_SHOW_RQUERY_DEVTOOLS", true)
