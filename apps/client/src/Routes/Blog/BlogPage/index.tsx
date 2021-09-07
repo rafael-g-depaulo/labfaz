d5cc92910f0f7f59c6f1971cb6810ffbb5fd7546
clarice42 <claramaia820@gmail.com>
2021-09-07 17:06:28 -0300

    ♻️ ✨ Add error handling on blog pages

--- apps/client/src/Routes/Blog/BlogPage/index.tsx
+++ apps/client/src/Routes/Blog/BlogPage/index.tsx
@@ -1,20 +1,42 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
 
-import LoadingFullPage from "Components/LoadingFullPage"
-import { useBlogBannerInfo } from "Api/BlogBannerInfo"
+import Display from "./Display";
 
-import Display from "./Display"
-import { useBlogPosts } from "Api/BlogPost"
+import Error from "Pages/Error";
+import LoadingFullPage from "Components/LoadingFullPage";
+
+import { mockImage } from "Utils/Image";
+
+import { useBlogPosts } from "Api/BlogPost";
+import { useBlogBannerInfo } from "Api/BlogBannerInfo";
 
 export const BlogPage: FC = () => {
-  const bannerInfo = useBlogBannerInfo()
-  const posts = useBlogPosts()
+  const bannerInfo = useBlogBannerInfo();
+  const posts = useBlogPosts();
+  const mockBannerInfo = {
+    title: "Blog",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: mockImage({
+      url: "https://labfaz-strapi-assets.s3.sa-east-1.amazonaws.com/Whats_App_Image_2020_12_19_at_17_23_28_439c4529a0.jpeg",
+      alternativeText: "Blog Banner Image",
+    }),
+  };
 
-  if (bannerInfo.isLoading || posts.isLoading) return <LoadingFullPage />
-  if (bannerInfo.error) return <div>error: {bannerInfo.error.message}</div>
-  if (posts.error) return <div>error: {posts.error.message}</div>
+  if (bannerInfo.isLoading || posts.isLoading) return <LoadingFullPage />;
 
-  return <Display data={bannerInfo.data} posts={posts.data} />
-}
+  if (posts.error) {
+    return (
+      <Error
+        errorStatus={posts.error.response?.status}
+        errorMessage={posts.error.response?.statusText}
+      />
+    );
+  }
 
-export default BlogPage
+  if (bannerInfo.error)
+    return <Display data={mockBannerInfo} posts={posts.data!} />;
+
+  return <Display data={bannerInfo.data} posts={posts.data} />;
+};
+
+export default BlogPage;
--- apps/client/src/Routes/Blog/PostPage/index.tsx
+++ apps/client/src/Routes/Blog/PostPage/index.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
 
-import LoadingFullPage from "Components/LoadingFullPage";
 import Display from "./Display";
+
+import Error from "Pages/Error";
+import LoadingFullPage from "Components/LoadingFullPage";
+
+import { mockImage } from "Utils/Image";
+
 import { useBlogBannerInfo } from "Api/BlogBannerInfo";
 import { useBlogPost } from "Api/BlogPost";
 
@@ -12,10 +17,27 @@ interface RouteParams {
 export const PostPage: FC<RouteParams> = ({ id }) => {
   const result = useBlogBannerInfo();
   const post = useBlogPost(id);
+  const mockBannerInfo = {
+    title: "Blog",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: mockImage({
+      url:
+        "https://labfaz-strapi-assets.s3.sa-east-1.amazonaws.com/Whats_App_Image_2020_12_19_at_17_23_28_439c4529a0.jpeg",
+      alternativeText: "Blog Banner Image",
+    }),
+  };
 
   if (result.isLoading || post.isLoading) return <LoadingFullPage />;
-  if (result.error) return <div>error: {result.error.message}</div>;
-  if (post.error) return <div>error: {post.error.message}</div>;
+
+  if (post.error)
+    return (
+      <Error
+        errorStatus={post.error.response?.status}
+        errorMessage={post.error.response?.statusText}
+      />
+    );
+
+  if (result.error) return <Display data={mockBannerInfo} post={post.data!} />;
 
   return <Display data={result.data} post={post.data} />;
 };
