dd9c5d8ac5534f3c3ccf70b9b1b0b6c49e5d43b8
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-09-10 21:25:37 -0300

    ♻️ Update create subscription request route

--- apps/api/src/Routes/Courses/index.ts
+++ apps/api/src/Routes/Courses/index.ts
@@ -2,26 +2,37 @@ import express from "express";
 import { Connection } from "typeorm";
 
 import { Router } from "Routes";
+
+import UserRepository from "Repository/UserRepository";
 import CourseRepository from "Repository/CourseRepository";
-// import ensureAuthenticated from "Middlewares/ensureAuthenticated";
+import RequestRepository from "Repository/RequestRepository";
+
+import ensureAuthenticated from "Middlewares/ensureAuthenticated";
 
 // import { CreateCourse } from "./CreateCourse";
 import { GetAllCourses as GetAllCourses } from "./GetAllCourses";
 import ShowCourse from "./ShowCourse";
-import { CreateCourse } from "./CreateCourse";
-
+import SubscribeToCourse from "./SubscribeToCourse";
 
 type CoureDeps = {
   conn: Connection;
   CourseRepo?: CourseRepository;
+  UserRepo?: UserRepository
+  RequestRepo?: RequestRepository
 };
 
 const CourseRoute: Router<CoureDeps> = (deps, options) => {
-  const { conn, CourseRepo = conn.getCustomRepository(CourseRepository) } = deps;
+  const {
+    conn,
+    CourseRepo = conn.getCustomRepository(CourseRepository),
+    UserRepo = conn.getCustomRepository(UserRepository),
+    RequestRepo = conn.getCustomRepository(RequestRepository),
+  } = deps;
 
   return express
     .Router(options)
     // .post("/create", CreateCourse({ CourseRepo }))
+    .post("/:course_id/subscribe", ensureAuthenticated, SubscribeToCourse({ CourseRepo, RequestRepo, UserRepo }))
     .get("/:course_id", ShowCourse({ CourseRepo }))
     .get("/", GetAllCourses({ CourseRepo }))
 }
