7555f4de769a58f2abc345dff6449f1372776955
Icaro Rodrigues <kevi_lothar@hotmail.com>
2021-09-14 15:59:26 -0300

    💄 Add new interface to user context

--- apps/client/src/Context/CurrentUser.tsx
+++ apps/client/src/Context/CurrentUser.tsx
@@ -2,6 +2,75 @@ import React, { FC, createContext, useMemo, useContext } from "react"
 
 import useLocalStorage from 'Hooks/useLocalStorage'
 
+export interface IAddress {
+  cep: string
+  city: string
+  complement: string
+  neighbourhood: string
+  number: number
+  residency: string
+  state: string
+}
+
+export interface IContact {
+  facebook: string
+  instagram: string
+  linkedin: string
+  tiktok: string
+  twitter: string
+  whatsapp: string
+  youtube: string
+}
+
+export interface ITechnicalIdioms {
+  name: string
+}
+
+export interface ITechnical {
+  ceac: string
+  cnpj: string
+  cnpj_type: string
+  drt: string
+  is_ceac: boolean
+  is_cnpj: boolean
+  is_drt: boolean
+  name_enterprise: string
+  profession: string
+  formation: string
+  idiom: ITechnicalIdioms[]
+  area: ITechnicalArea[]
+}
+
+export interface ITechnicalAreaCertificate {
+  name: string
+}
+export interface ITechnicalArea {
+  describe: string
+  name: string
+  started_year: string
+  technical_formation: string
+  certificate: ITechnicalAreaCertificate[]
+}
+
+export interface IArtist {
+  name: string
+  artistic_name: string
+  social_name: string
+  show_name: string
+  birthday: Date
+  photo_url: string
+  curriculum: string
+  cpf: string
+  rg: string
+  sexual_orientation: string
+  gender: string
+  race: string
+  gender_specifics: string
+  expedition_department: string
+  address: IAddress
+  contact: IContact
+  technical: ITechnical
+}
 export interface User {
   id: string
   email: string
@@ -10,6 +79,7 @@ export interface User {
   active: boolean
   updated_at: Date
   created_at: Date
+  artist: IArtist
 }
 
 export interface CurrentUser {
