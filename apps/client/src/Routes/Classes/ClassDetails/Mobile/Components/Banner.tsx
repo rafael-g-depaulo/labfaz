9b5cf8fb0776e1e23f60865f4903919c1d5ad6ee
zipudhe <lucasramalho29@gmail.com>
2021-09-15 19:52:23 -0300

    ✨ add fallback image to banner

--- apps/client/src/Routes/Classes/ClassDetails/Mobile/Components/Banner.tsx
+++ apps/client/src/Routes/Classes/ClassDetails/Mobile/Components/Banner.tsx
@@ -8,11 +8,11 @@ export interface BannerProps {
 
 export const Banner: FC<BannerProps> = ({ img }) => {
 
-  console.log(img)
+  const fallbackImage = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG"
 
   return (
     <BannerDiv>
-      <Image src={img} />
+      <Image src={img || fallbackImage} />
     </BannerDiv>
   )
 }
