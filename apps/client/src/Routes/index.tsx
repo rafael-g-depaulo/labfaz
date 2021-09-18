a014e24b85e8a2d6c34f1d10d365d807afcdc748
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-09-18 13:39:52 -0300

    💬 Update route

--- apps/client/src/Routes/index.tsx
+++ apps/client/src/Routes/index.tsx
@@ -137,7 +137,7 @@ const Routes: FC = () => {
         </Route>
 
         {/* recover router */}
-        <Route path={["/recover", "/forgot-password", "/criar-nova-senha"]}>
+        <Route path={["/recover", "/forgot-password", "/password-reset","/criar-nova-senha"]}>
           {({ match }) => (
             <Suspense fallback={<LoadingFullPage />}>
               <Recover match={match} />
