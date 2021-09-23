3caeb41f4c87ae9d47b776b9e127ddce04adf9d2
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-09-23 13:23:57 -0300

    🔧 use env var for JWT secret

--- apps/api/src/Config/auth.ts
+++ apps/api/src/Config/auth.ts
@@ -1,12 +1,12 @@
 export default {
 
   jwt: {
-    secret: '8020877602e2d69c02c2f0377909b8f2',
+    secret: process.env.JWT_SECRET ?? '8020877602e2d69c02c2f0377909b8f2',
     expiresIn: '1d'
   },
 
   token: {
-    secret: '8020877602e2d69c02c2f0377909b8f2',
+    secret: process.env.JWT_SECRET ?? '8020877602e2d69c02c2f0377909b8f2',
     expiresIn: '600s'
   }
 }
