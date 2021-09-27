18ae4b39cf7b2264fc10c94c9202af10c0093976
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-09-27 11:53:36 -0300

    ✨ Add useTimeout hook

---
+++ apps/client/src/Hooks/useTimeout.ts
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+
+const useTimeout = (ms: number, autoStart = false) => {
+
+  const timeout = useRef<number | null>(null)
+
+  const [done, setDone] = useState(true)
+
+  const onFinish = useCallback(() => setDone(true), [setDone])
+
+  const start = useCallback(() => {
+    if (timeout.current !== null) return
+  
+    setDone(false)
+    timeout.current = setTimeout(onFinish, ms)
+  }, [onFinish])
+
+  const stop = useCallback(() => {
+    if (timeout.current === null) return
+
+    setDone(true)
+    clearTimeout(timeout.current)
+  }, [])
+
+  // start timeout on first render if autoStart prop given
+  // TODO: maybe add a state to make sure this is only called on first render?
+  useEffect(() => {
+    if (autoStart) start()
+  }, [start, autoStart])
+
+  return { start, stop, done }
+}
+
+export default useTimeout
